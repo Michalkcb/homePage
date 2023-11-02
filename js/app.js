78970e14d02101f91ee4139845faeea6a20f488d
Michalkcb <michalkcb@gmail.com>
2023-11-02 16:57:20 +0100

    transform thml projects from html to js

--- js/app.js
+++ js/app.js
@@ -93,7 +93,64 @@ for (let i = 0; i < certificatesData.length; i++) {
     boxDiv.appendChild(spanElem);
     certificatesDiv.appendChild(boxDiv);
 }
+//HTML projects
 
+const projectsData = [
+  {
+    link: './projectsHtml/glowingIcons/index.html',
+    imageSrc: './assets/glowingItems.jpg',
+    alt: 'glowing_icons'
+  },
+  {
+    link: 'http://michalkcb.ct8.pl/new%20cruzeirosazuis/newTerminyPL.html',
+    imageSrc: 'http://michalkcb.ct8.pl/pic/cruseiro.jpg',
+    alt: 'page_pic1'
+  },
+  {
+    link: 'http://michalkcb.ct8.pl/glowingDiv/index.html',
+    imageSrc: 'http://michalkcb.ct8.pl/glowingDiv/glowingDivs.png',
+    alt: 'Glowing DIV'
+  },
+  {
+    link: '../old/index.html',
+    imageSrc: 'http://michalkcb.ct8.pl/pic/old.png',
+    alt: 'old WD'
+  },
+  {
+    link: 'http://michalkcb.ct8.pl/page07/page07.html',
+    imageSrc: 'http://michalkcb.ct8.pl/pic/page07.png',
+    alt: 'page_pic7'
+  },
+  {
+    link: 'http://michalkcb.ct8.pl/EuroChina/index.html',
+    imageSrc: 'http://michalkcb.ct8.pl/pic/page02.png',
+    alt: 'page_pic2'
+  },
+  {
+    link: 'http://michalkcb.ct8.pl/page06/page06.html',
+    imageSrc: 'http://michalkcb.ct8.pl/pic/page06.png',
+    alt: 'page_pic6'
+  }
+];
+
+const projectsContainer = document.querySelector('.recent');
+
+projectsData.forEach(project => {
+  const projectDiv = document.createElement('div');
+  projectDiv.classList.add('example');
+
+  const projectLink = document.createElement('a');
+  projectLink.href = project.link;
+  projectLink.target = '_blank';
+
+  const projectImage = document.createElement('img');
+  projectImage.src = project.imageSrc;
+  projectImage.alt = project.alt;
+
+  projectLink.appendChild(projectImage);
+  projectDiv.appendChild(projectLink);
+  projectsContainer.appendChild(projectDiv);
+});
 
 // Resume
 const resume = document.createElement("div");
