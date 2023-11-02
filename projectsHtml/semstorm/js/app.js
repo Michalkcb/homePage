a44b7e4d686b9ca2c5cbbfcd44dbbf4d1a400d62
Michalkcb <michalkcb@gmail.com>
2023-11-02 15:25:44 +0100

    text position change

--- projectsHtml/semstorm/js/app.js
+++ projectsHtml/semstorm/js/app.js
@@ -12,16 +12,21 @@ sidebar.appendChild(sidebarElement);
 const main = document.querySelector('.main');
 const mainElement = document.createElement('h3');
 mainElement.innerHTML = 'Motivation Letter <img src="./assets/logo-dark.png">';
+main.classList.add('round-corners')
 main.appendChild(mainElement);
 
 const section = document.querySelector('.section');
 const sectionlement = document.createElement('p');
-sectionlement.textContent = 'Jeszcze na początku lat 2000 w trakcie studiow zacząłem twrzyć strony www aby dorobić trochę na studiach. Stworzyłem też własny serwis www.playstation2.pl który prowadziłem przez 2 lata. Po studiach w firmie Albertech zajmowałem sie tworzeniem i administracją firmowej strony www. Potem moje drogi z programowaniem się rozeszły. Powróciłem jednak do mojej starej pasii. Ukończyłem w tym roku kilka kursów Front-End na studiuje.it, udemy oraz University of California: Davis.   ';
+sectionlement.innerHTML = 'Jeszcze na początku lat 2000 w trakcie studiow zacząłem twrzyć strony www aby dorobić trochę na studiach. Stworzyłem też własny serwis www.playstation2.pl który prowadziłem przez 2 lata. Po studiach w firmie Albertech zajmowałem sie tworzeniem i administracją firmowej strony www. A moja strona www została zapomniana. Potem moje drogi z programowaniem się rozeszły.<br> <br>Powróciłem jednak do mojej starej pasii. Ukończyłem w tym roku kilka kursów Front-End na studiuje.it, udemy oraz University of California: Davis.   ';
+section.classList.add('round-corners')
+
 section.appendChild(sectionlement);
 
 const content = document.querySelector('.content');
 const contentElement = document.createElement('h3');
-contentElement.textContent = 'Motivation Letter SEMSTORM';
+contentElement.textContent = 'Uważam, że moje umiejętności odpowiadają, tym które zostały umieszczone w ogłoszeniu na Front-end Developer na stronie https://theprotocol.it. Zachęcam do odwiedzenia linków po lewej stronie do mojej domowej strony oraz moich projektów na GitHub. Wszystkie te rzeczy zostały przeze mnie napisane przy użyciu VisualStudioCode i zacomitowane za pomocą Git do Github. ';
+content.classList.add('round-corners')
+
 content.appendChild(contentElement);
 
 const footer = document.querySelector('.footer');
