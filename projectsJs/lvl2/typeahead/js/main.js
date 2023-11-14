c6098a2cd5910befedc1b8152b559e760b2ac64f
Michalkcb <michalkcb@gmail.com>
2023-11-14 17:34:01 +0100

    CHANGE JSON FILE CONTENT, ADD HTML OUTPUT

--- projectsJs/lvl2/typeahead/js/main.js
+++ projectsJs/lvl2/typeahead/js/main.js
@@ -8,10 +8,29 @@ const searchCountry = async (searchText) => {
   //dopadowanie do input
   let matches = countries.filter(country => {
     const regex = new RegExp(`^${searchText}`, 'gi');
-    return country.country.match(regex) || country.alpha2.match(regex);
+    return country.countryName.match(regex) || country.capital.match(regex);
   });
-console.log(matches)
 
+  if(searchText.length === 0) {
+    matches = [];
+    matchList.innerHTML = '';
+  }
+console.log(matches);
+  outptHtml(matches)
 };
+
+const outptHtml = matches => {
+  if(matches.length > 0) {
+    const html = matches.map(match => `
+      <div class="card card-body mb-1">
+        <h4>${match.countryName} (${match.capital}) 
+        <span class="text-primary">Population: ${match.population} </span></h4>
+        <small> Continent: ${match.continentName}</small>
+      </div>
+    `).join('');
+
+    matchList.innerHTML = html;
+  }
+}
 
 search.addEventListener("input", () => searchCountry(search.value));
