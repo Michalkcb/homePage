432dae6bb7d9bafa88578ac2b2f731159c511266
Michalkcb <michalkcb@gmail.com>
2023-12-30 17:11:35 +0100

    add imgs

--- js/app.js
+++ js/app.js
@@ -104,47 +104,14 @@ const certificatesData = [
 //HTML projects
 
 const projectsData = [
-  {
-    link: './projectsHtml/glowingIcons/index.html',
-    imageSrc: './assets/glowingItems.jpg',
-    alt: 'glowing_icons',
-  },
-  {
-    link: 'http://michalkcb.ct8.pl/new%20cruzeirosazuis/newTerminyPL.html',
-    imageSrc: 'http://michalkcb.ct8.pl/pic/cruseiro.jpg',
-    alt: 'page_pic1'
-  },
-  {
-    link: 'http://michalkcb.ct8.pl/glowingDiv/index.html',
-    imageSrc: 'http://michalkcb.ct8.pl/glowingDiv/glowingDivs.png',
-    alt: 'Glowing DIV'
-  },
-  {
-    link: '../old/index.html',
-    imageSrc: 'http://michalkcb.ct8.pl/pic/old.png',
-    alt: 'old WD'
-  },
-  {
-    link: 'http://michalkcb.ct8.pl/page07/page07.html',
-    imageSrc: 'http://michalkcb.ct8.pl/pic/page07.png',
-    alt: 'page_pic7'
-  },
-  {
-    link: 'http://michalkcb.ct8.pl/EuroChina/index.html',
-    imageSrc: 'http://michalkcb.ct8.pl/pic/page02.png',
-    alt: 'page_pic2'
-  },
-  {
-    link: 'http://michalkcb.ct8.pl/page06/page06.html',
-    imageSrc: 'http://michalkcb.ct8.pl/pic/page06.png',
-    alt: 'page_pic6'
-  },
-  {
-    link: 'https://michalkcb.github.io/homePage/projectsHtml/semstorm/index.html',
-    imageSrc: './projectsHtml/semstorm/assets/semstorm.png',
-    alt: 'semstorm'
-  }
-
+  {link: './projectsHtml/glowingIcons/index.html', imageSrc: './assets/glowingItems.jpg', alt: 'glowing_icons'},
+  {link: 'http://michalkcb.ct8.pl/new%20cruzeirosazuis/newTerminyPL.html',imageSrc: 'http://michalkcb.ct8.pl/pic/cruseiro.jpg',alt: 'page_pic1'},
+  {link: 'http://michalkcb.ct8.pl/glowingDiv/index.html',imageSrc: 'http://michalkcb.ct8.pl/glowingDiv/glowingDivs.png',alt: 'Glowing DIV'},
+  {link: '../old/index.html',imageSrc: 'http://michalkcb.ct8.pl/pic/old.png',alt: 'old WD'},
+  {link: 'http://michalkcb.ct8.pl/page07/page07.html',imageSrc: 'http://michalkcb.ct8.pl/pic/page07.png',alt: 'page_pic7'},
+  {link: 'http://michalkcb.ct8.pl/EuroChina/index.html',imageSrc: 'http://michalkcb.ct8.pl/pic/page02.png',alt: 'page_pic2'},
+  {link: 'http://michalkcb.ct8.pl/page06/page06.html',imageSrc: 'http://michalkcb.ct8.pl/pic/page06.png',alt: 'page_pic6'},
+  {link: 'https://michalkcb.github.io/homePage/projectsHtml/semstorm/index.html',imageSrc: './projectsHtml/semstorm/assets/semstorm.png',alt: 'semstorm'  }
 ];
 
 const recent = document.querySelector('.recent');
