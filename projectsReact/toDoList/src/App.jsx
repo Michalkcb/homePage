78bb0739014cf2ad65058522b25477f6a6544737
Michalkcb <michalkcb@gmail.com>
2024-01-22 22:21:32 +0100

    react vite build done

--- projectsReact/toDoList/src/App.jsx
+++ projectsReact/toDoList/src/App.jsx
@@ -43,6 +43,7 @@ export default function App() {
     <>
       <NewTodoForm onSubmit={addTodo} />
       <h1 className="header">Todo List</h1>
+      <h2 className="header2">React + Vite + localStorage</h2>
       <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   )
