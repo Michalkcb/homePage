ce9904555ce3457771969aba928232a0f411e048
Michalkcb <michalkcb@gmail.com>
2024-01-22 17:09:17 +0100

    add list items

--- projectsReact/toDoList/src/App.jsx
+++ projectsReact/toDoList/src/App.jsx
@@ -1,9 +1,32 @@
 import "./styles.css"
 export default function App() {
-  return <form className="new-item-form">
+  return (
+    <>
+  <form className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
       <input type="text" id="item" />
     </div>
+    <button className="btn">Add</button>
   </form>
+  <h1 className="header"> Todo List</h1>
+  <ul className="list">
+    <li>
+      <label>
+        <input type="checkbox"/>
+        Item 1
+      </label>
+      <button className="btn btn-danger">Delete</button>
+    </li>
+    <li>
+      <label>
+        <input type="checkbox"/>
+        Item 2
+      </label>
+      <button className="btn btn-danger">Delete</button>
+    </li>
+  </ul>
+  </>
+  )
+
 }
