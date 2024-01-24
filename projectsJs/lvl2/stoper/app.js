2255c46ee27323e63abbb841f7397df919c371c5
Michalkcb <michalkcb@gmail.com>
2024-01-24 20:01:16 +0100

    change to adevent listener

--- projectsJs/lvl2/stoper/app.js
+++ projectsJs/lvl2/stoper/app.js
@@ -1,29 +1,38 @@
 let timer;
 let seconds = 0;
-const startBtn = document.querySelector('start');
-const pauseBtn = document.querySelector('pause');
-const resetBtn = document.querySelector('reset');
+const startBtn = document.querySelector('.start');
+const pauseBtn = document.querySelector('.pause');
+const resetBtn = document.querySelector('.reset');
+
+startBtn.addEventListener('click', startTimer);
+pauseBtn.addEventListener('click', pauseTimer);
+resetBtn.addEventListener('click', resetTimer);
 
 
 function startTimer() {
+    if (!timer){
   timer = setInterval(updateTimer, 1000);
+    }
 }
 
 function pauseTimer() {
   clearInterval(timer);
+  timer = null
 }
 
 function resetTimer() {
   clearInterval(timer);
+  timer = null;
   seconds = 0;
   updateTimer();
 }
 
 function updateTimer() {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
-  document.getElementById("timer").innerText = `${minutes}:${
-    remainingSeconds < 10 ? "0" : ""
-  }${remainingSeconds}`;
+  document.getElementById("timer").innerText = `${minutes}:$${remainingSeconds}`;
+//   document.getElementById("timer").innerText = `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
   seconds++;
 }
