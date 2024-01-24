b42c69f6b2813596d7054a97159ef723d32140da
Michalkcb <michalkcb@gmail.com>
2024-01-24 17:11:31 +0100

    prettier js

--- projectsJs/lvl2/ticTacToe/app.js
+++ projectsJs/lvl2/ticTacToe/app.js
@@ -1,58 +1,63 @@
-const PLAYER1 = 'O';
-const PLAYER2 = 'X';
+const PLAYER1 = "O";
+const PLAYER2 = "X";
 let round = 1;
 let gameWon = false;
 const board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
 ];
 const combinations = [
-    [0,1,2], [3,4,5], [6,7,8],
-    [0,3,6], [1,4,7], [2,5,8],
-    [0,4,8], [2,4,6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
-const boxes = document.querySelectorAll('.box');
+const boxes = document.querySelectorAll(".box");
 arrBoxes = [...boxes];
 
-const pick = (e)=> {
-    if (gameWon) return;
+const pick = (e) => {
+  if (gameWon) return;
   const row = parseInt(e.target.dataset.row);
-    const col = parseInt(e.target.dataset.column);
+  const col = parseInt(e.target.dataset.column);
 
+  const turn = round % 2 === 0 ? PLAYER2 : PLAYER1;
 
-    const turn = round % 2 === 0 ? PLAYER2 : PLAYER1;
+  if (board[row][col] !== "") return;
+  e.target.textContent = turn;
+  board[row][col] = turn;
+  round++;
 
-    if (board[row][col] !== '') return;
-    e.target.textContent = turn;
-    board[row][col] = turn;
-    round++;
+  console.log(check());
+};
+arrBoxes.forEach((box) => box.addEventListener("click", pick));
 
-    console.log(check());
-}
-arrBoxes.forEach(box => box.addEventListener('click', pick));
-
-
-const check =() =>{
-    const result = board.reduce((total, row) => total.concat(row));
-    let winner = null;
-    let moves = {
-        'X': [],
-        'O': []
-    };
-    result.forEach((field, index) => moves[field] ? moves[field].push(index) : null);
-    combinations.forEach(combination => {
-        if (combination.every(index => moves[PLAYER1].indexOf(index) > -1)) {
-            winner = 'Player O';
-        }
-        if (combination.every(index => moves[PLAYER2].indexOf(index) > -1)) {
-            winner = 'Player X';
-        }
-    });
-
-    if (winner) {
-        document.getElementById('winner-name').textContent = winner;
-        gameWon = true;
+const check = () => {
+  const result = board.reduce((total, row) => total.concat(row));
+  let winner = null;
+  let moves = {
+    X: [],
+    O: [],
+  };
+  result.forEach((field, index) =>
+    moves[field] ? moves[field].push(index) : null
+  );
+  combinations.forEach((combination) => {
+    if (combination.every((index) => moves[PLAYER1].indexOf(index) > -1)) {
+      winner = "Player O";
     }
-}
+    if (combination.every((index) => moves[PLAYER2].indexOf(index) > -1)) {
+      winner = "Player X";
+    }
+  });
+
+  if (winner) {
+    document.getElementById("winner-name").textContent = winner;
+    gameWon = true;
+  }
+};
