828d8edb0556a72417d58f1cfe39e7f72fd904d0
Michalkcb <michalkcb@gmail.com>
2024-01-24 19:24:18 +0100

    delete onclick

--- projectsJs/lvl2/stoper/app.js
+++ projectsJs/lvl2/stoper/app.js
@@ -1,23 +1,29 @@
 let timer;
-  let seconds = 0;
+let seconds = 0;
+const startBtn = document.querySelector('start');
+const pauseBtn = document.querySelector('pause');
+const resetBtn = document.querySelector('reset');
 
-  function startTimer() {
-    timer = setInterval(updateTimer, 1000);
-  }
 
-  function pauseTimer() {
-    clearInterval(timer);
-  }
+function startTimer() {
+  timer = setInterval(updateTimer, 1000);
+}
 
-  function resetTimer() {
-    clearInterval(timer);
-    seconds = 0;
-    updateTimer();
-  }
+function pauseTimer() {
+  clearInterval(timer);
+}
 
-  function updateTimer() {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-    document.getElementById('timer').innerText = `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
-    seconds++;
-  }
+function resetTimer() {
+  clearInterval(timer);
+  seconds = 0;
+  updateTimer();
+}
+
+function updateTimer() {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  document.getElementById("timer").innerText = `${minutes}:${
+    remainingSeconds < 10 ? "0" : ""
+  }${remainingSeconds}`;
+  seconds++;
+}
