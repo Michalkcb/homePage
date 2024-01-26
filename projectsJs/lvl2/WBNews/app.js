85653318e9bd7044f2e6b50c97328f69da33f037
Michalkcb <michalkcb@gmail.com>
2024-01-26 20:09:30 +0100

    add articlecard

--- projectsJs/lvl2/WBNews/app.js
+++ projectsJs/lvl2/WBNews/app.js
@@ -6,10 +6,33 @@ const options = ["general","entertainment", "business", "health", "science", "sp
 let requestURL;
 
 const generateUI = (articles) => {
-
+    for(let item of articles) {
+        let card = document.createElement('div');
+        card.classList.add('news-card');
+        card.innerHTML = `<div class="news-image-container">
+        <img src="${item.urlToImage || ".newspaper.jpg"} " alt="" />
+        </div>
+        <div class="news-content">
+            <div class="news-title">
+                ${item.title}
+                </div>
+                <div class="news-description">
+                    ${item.description || item.content || ""}
+                </div>
+                <a href="${item.url}" target="_blank" class="viev-button">Read More</a>
+        </div>`;
+        container.appendChild(card);
+    }
 }
 
 const init = ()=> {
     optionContainer.innerHTML = "";
-    
+    getNews();
+    createOptions();
+
+};
+
+window.onload = () => {
+    requestURL = `https://newsapi.org/v2/top-headlines?country=${country}&category=general&apiKey=${apiKey}`;
+    init();
 }
