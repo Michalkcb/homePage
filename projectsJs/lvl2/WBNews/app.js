a55bf4023673955834cc0810289f0428b972cfd0
Michalkcb <michalkcb@gmail.com>
2024-01-26 19:13:32 +0100

    first js lines

--- projectsJs/lvl2/WBNews/app.js
+++ projectsJs/lvl2/WBNews/app.js
@@ -1,1 +1,15 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
+const optionContainer = document.querySelector('.opt-container')
+const country = "pl";
+const options = ["general","entertainment", "business", "health", "science", "sports", "technology"];
+
+let requestURL;
+
+const generateUI = (articles) => {
+
+}
+
+const init = ()=> {
+    optionContainer.innerHTML = "";
+    
+}
