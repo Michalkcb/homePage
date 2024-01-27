1c2b36202fd58068db961579b7dc975e349c152a
Michalkcb <michalkcb@gmail.com>
2024-01-27 15:18:37 +0100

    img works

--- projectsJs/lvl2/WBNews/app.js
+++ projectsJs/lvl2/WBNews/app.js
@@ -11,7 +11,7 @@ const generateUI = (articles) => {
       let card = document.createElement("div");
       card.classList.add("news-card");
       card.innerHTML = `<div class="news-image-container">
-      <img src="${item.urlToImage}" alt="" />
+      <img src="${item.image}" alt="" />
       </div>
       <div class="news-content">
         <div class="news-title">
@@ -23,7 +23,7 @@ const generateUI = (articles) => {
         <a href="${item.url}" target="_blank" class="view-button">Read More</a>
       </div>`;
       container.appendChild(card);
-      console.log(item.urlToImage)
+      console.log(item.image)
     }
   };
 
