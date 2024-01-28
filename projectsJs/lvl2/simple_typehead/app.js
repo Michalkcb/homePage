67909873a6351aa2d745c8662e174d657acd46dc
Michalkcb <michalkcb@gmail.com>
2024-01-28 20:08:43 +0100

    typeahad done

--- projectsJs/lvl2/simple_typehead/app.js
+++ projectsJs/lvl2/simple_typehead/app.js
@@ -1,18 +1,33 @@
+const optionsArray = [
+  "pies",
+  "kot",
+  "tchórzofretka",
+  "ser",
+  "kapeć",
+  "portki",
+  "majty",
+  "onuce",
+];
+const datalist = document.querySelector("#sth");
+const input = document.querySelector("input");
 
-  const optionsArray = [
-    "pies",
-    "kot",
-    "tchórzofretka",
-    "ser",
-    "kapeć",
-    "portki",
-    "majty",
-    "onuce",
-  ];
-  const datalist = document.querySelector("#sth");
+optionsArray.forEach((optionValue) => {
+  const optionElement = document.createElement("option");
+  optionElement.value = optionValue;
+  datalist.appendChild(optionElement);
+});
 
-  optionsArray.forEach((optionValue)=> {
-    const optionElement = document.createElement("option");
+input.addEventListener('input', ()=>{
+const inputValue = input.value.toLocaleLowerCase();
+
+datalist.innerHTML = '';
+
+if (inputValue.length >= 1){
+optionsArray.filter(option => option.toLocaleLowerCase().startsWith(inputValue)).forEach((optionValue)=>{
+    const optionElement = document.createElement('option');
     optionElement.value = optionValue;
     datalist.appendChild(optionElement);
-  });
+});
+};
+
+});
