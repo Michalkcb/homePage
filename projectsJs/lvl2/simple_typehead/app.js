0fc313d68c2c5f02706c6870c233df28b34d3cb0
Michalkcb <michalkcb@gmail.com>
2024-01-28 19:12:22 +0100

    ja working

--- projectsJs/lvl2/simple_typehead/app.js
+++ projectsJs/lvl2/simple_typehead/app.js
@@ -1,2 +1,18 @@
-const searchWrapper = document.querySelector('.search-input');
-const inputBox = searchWrapper.querySelector('input')
+
+  const optionsArray = [
+    "pies",
+    "kot",
+    "tchórzofretka",
+    "ser",
+    "kapeć",
+    "portki",
+    "majty",
+    "onuce",
+  ];
+  const datalist = document.getElementById("sth");
+
+  optionsArray.forEach(function (optionValue) {
+    const optionElement = document.createElement("option");
+    optionElement.value = optionValue;
+    datalist.appendChild(optionElement);
+  });
