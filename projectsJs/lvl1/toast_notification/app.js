5b2f0ffc13b8350ed440796701f3795602668220
Michalkcb <michalkcb@gmail.com>
2024-01-29 16:16:05 +0100

    add querry selectors

---
+++ projectsJs/lvl1/toast_notification/app.js
@@ -0,0 +1,5 @@
+const notifications = document.querySelector('.notifications');
+const succes = document.querySelector('.succes');
+const error = document.querySelector('.error');
+const info = document.querySelector('.info');
+const warning = document.querySelector('.warning');
