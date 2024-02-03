7a9ff77e36851c55c01aade50350d42069a49fb9
Michalkcb <michalkcb@gmail.com>
2024-02-03 21:45:55 +0100

    semstorm commented

--- js/modules/html.js
+++ js/modules/html.js
@@ -6,7 +6,7 @@ const projectsData = [
     {link: 'http://michalkcb.ct8.pl/page07/page07.html',imageSrc: 'http://michalkcb.ct8.pl/pic/page07.png',alt: 'page_pic7'},
     {link: 'http://michalkcb.ct8.pl/EuroChina/index.html',imageSrc: 'http://michalkcb.ct8.pl/pic/page02.png',alt: 'page_pic2'},
     {link: 'http://michalkcb.ct8.pl/page06/page06.html',imageSrc: 'http://michalkcb.ct8.pl/pic/page06.png',alt: 'page_pic6'},
-    {link: 'https://michalkcb.github.io/homePage/projectsHtml/semstorm/index.html',imageSrc: 'https://michalkcb.github.io/homePage/projectsHtml/semstorm/assets/semstorm.png',alt: 'semstorm'  }
+    // {link: 'https://michalkcb.github.io/homePage/projectsHtml/semstorm/index.html',imageSrc: 'https://michalkcb.github.io/homePage/projectsHtml/semstorm/assets/semstorm.png',alt: 'semstorm'  }
   ];
 
   export default projectsData;
