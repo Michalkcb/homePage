d4ed44141e2f3c59072a62a27c6b6a4f25bbb34d
Michalkcb <michalkcb@gmail.com>
2024-02-06 20:51:03 +0100

    styling main

--- projectsReact/reactPortfolio/src/components/Main.jsx
+++ projectsReact/reactPortfolio/src/components/Main.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
-import { FaFacebookF, FaLinkedinIn, FaTwitter, FaInstagram} from "react-icons/fa";
+import {
+  FaFacebookF,
+  FaLinkedinIn,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
 
 const Main = () => {
   return (
@@ -12,8 +17,8 @@ const Main = () => {
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
         <div className="max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center">
-          <h1>I'm Mick</h1>
-          <h2>
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800" >I'm Michael Bany</h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800" >
             I'm a
             <TypeAnimation
               sequence={[
@@ -33,10 +38,10 @@ const Main = () => {
             />
           </h2>
           <div>
-            <FaFacebookF className='cursor-pointer' size={20}/>
-            <FaLinkedinIn className='cursor-pointer' size={20}/>
-            <FaInstagram className='cursor-pointer' size={20}/>
-            <FaTwitter className='cursor-pointer' size={20}/>
+            <FaFacebookF className="cursor-pointer" size={20} />
+            <FaLinkedinIn className="cursor-pointer" size={20} />
+            <FaInstagram className="cursor-pointer" size={20} />
+            <FaTwitter className="cursor-pointer" size={20} />
           </div>
         </div>
       </div>
