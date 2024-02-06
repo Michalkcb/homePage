c6abf69831ca831c3e3cb28b10f8bc9a58e8744c
Michalkcb <michalkcb@gmail.com>
2024-02-06 19:45:49 +0100

    add npm install react-type-animation

--- projectsReact/reactPortfolio/src/App.jsx
+++ projectsReact/reactPortfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Sidenav from './components/Sidenav'
+import Main from './components/Main'
 
 function App() {
 
   return (
     <>
 <Sidenav/>
+<Main/>
     </>
   )
 }
--- projectsReact/reactPortfolio/src/components/Main.jsx
+++ projectsReact/reactPortfolio/src/components/Main.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Main = () => {
   return (
     <div id='main'>
-        <img src="" alt="" />
+        <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="https://michalkcb.github.io/homePage/assets/codeBG2.png" alt="" />
+        <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
+          <div>
+            <h1>I'm Mick</h1>
+            <h2>I'm a Frondend Developer</h2>
+          </div>
+        </div>
     </div>
   )
 }
