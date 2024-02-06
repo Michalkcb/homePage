73b9f02e095ce1f5b78cd3c1be7e987490e32285
Michalkcb <michalkcb@gmail.com>
2024-02-06 20:06:55 +0100

    styling main

--- projectsReact/reactPortfolio/src/components/Main.jsx
+++ projectsReact/reactPortfolio/src/components/Main.jsx
@@ -1,17 +1,47 @@
-import React from 'react'
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import { FaFacebookF, FaLinkedinIn, FaTwitter, FaInstagram} from "react-icons/fa";
 
 const Main = () => {
   return (
-    <div id='main'>
-        <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="https://michalkcb.github.io/homePage/assets/codeBG2.png" alt="" />
-        <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
+    <div id="main">
+      <img
+        className="w-full h-screen object-cover object-left scale-x-[-1]"
+        src="https://michalkcb.github.io/homePage/assets/codeBG2.png"
+        alt=""
+      />
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+        <div className="max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center">
+          <h1>I'm Mick</h1>
+          <h2>
+            I'm a
+            <TypeAnimation
+              sequence={[
+                "Developer",
+                1000,
+                "Coder",
+                1000,
+                "Tech entusiast",
+                1000,
+                "Constant learner",
+                1000,
+              ]}
+              wrapper="div"
+              cursor={true}
+              style={{ fontSize: "1em", paddingLeft: "5px" }}
+              repeat={Infinity}
+            />
+          </h2>
           <div>
-            <h1>I'm Mick</h1>
-            <h2>I'm a Frondend Developer</h2>
+            <FaFacebookF className='cursor-pointer' size={20}/>
+            <FaLinkedinIn className='cursor-pointer' size={20}/>
+            <FaInstagram className='cursor-pointer' size={20}/>
+            <FaTwitter className='cursor-pointer' size={20}/>
           </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
