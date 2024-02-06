0983addae506d3aabf28fbd5551a7bb6a01f0669
Michalkcb <michalkcb@gmail.com>
2024-02-06 16:08:16 +0100

    fighting with tailwind configuration

--- projectsReact/reactPortfolio/src/components/Sidenav.jsx
+++ projectsReact/reactPortfolio/src/components/Sidenav.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React , {useState} from 'react'
 import {AiOutlineMenu} from 'react-icons/ai'
 
 const Sidenav = () => {
+    const[nav, setNav] = useState(false)
+    const handleNav = () => {
+        setNav(!nav);
+    }
   return (
     <>
-    <AiOutlineMenu/>
+    <AiOutlineMenu className='absolute top-4 right-4'/>
+
+
     </>
   )
 }
