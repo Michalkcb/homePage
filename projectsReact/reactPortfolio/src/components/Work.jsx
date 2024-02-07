2916cd3df6996a7bcc3eb55d9215ebe122f02849
Michalkcb <michalkcb@gmail.com>
2024-02-07 18:46:03 +0100

    work component working

--- projectsReact/reactPortfolio/src/components/Work.jsx
+++ projectsReact/reactPortfolio/src/components/Work.jsx
@@ -24,17 +24,17 @@ const data = [
 
 const Work = () => {
   return (
-    <div id="work" className="max-w-[1-4-px] m-auto md:pl-20 p-4 py-16">
+    <div id="work" className="max-w-[1040-px] m-auto md:pl-20 p-4 py-16">
       <h1 className="text-4xl font-bold text-center text-[#001b5e]">Work</h1>
-      {data.map((item, idx) => {
+      {data.map((item, idx) => (
         <WorkItem
           key={idx}
           year={item.year}
           title={item.title}
           duration={item.duration}
           details={item.details}
-        />;
-      })}
+        />
+      ))}
     </div>
   );
 };
