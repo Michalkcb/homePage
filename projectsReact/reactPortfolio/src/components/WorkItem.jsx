c61f243ab39453229d3184ea6ff220998bb92216
Michalkcb <michalkcb@gmail.com>
2024-02-07 18:54:01 +0100

    init content workitem

--- projectsReact/reactPortfolio/src/components/WorkItem.jsx
+++ projectsReact/reactPortfolio/src/components/WorkItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const WorkItem = () => {
   return (
-    <div>WorkItem</div>
+    <ol className='flex flex-col md:flex-row relative border-l border-stone-200' >WorkItem</ol>
   )
 }
 
