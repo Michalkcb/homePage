d8d8c4901b6fc6d20175296f5415cf6f8e83fe8b
Michalkcb <michalkcb@gmail.com>
2024-02-07 20:15:04 +0100

    styling wotk item done

--- projectsReact/reactPortfolio/src/components/WorkItem.jsx
+++ projectsReact/reactPortfolio/src/components/WorkItem.jsx
@@ -6,11 +6,11 @@ const WorkItem = ({ year, title, duration, details }) => {
       <li className="mb-10 ml-4" >
         <div className="absolute w-4 h-4 bg-stone-200 rounded-full mt-2 -left-2 border-white" />
         <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-xs" >
-          <span>{year}</span>
-          <span>{title}</span>
-          <span>{duration}</span>
+          <span className="inline-block px-2 py-1 font-semibold text-white bg-[#001b53] rounded-md " >{year}</span>
+          <span className="text-lg font-semibold text-[#001b5e]" >{title}</span>
+          <span className="my-1 text-sm font-normal leading-none text-stone-400" >{duration}</span>
         </p>
-        <p>{details}</p>
+        <p className="my-2 text-base font-normal text-stone-500" >{details}</p>
       </li>
     </ol>
   );
