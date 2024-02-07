6af9b53cb3f6d6c54d20df85e5a11ab9b748f014
Michalkcb <michalkcb@gmail.com>
2024-02-07 20:19:41 +0100

    init project item component

--- projectsReact/reactPortfolio/src/App.jsx
+++ projectsReact/reactPortfolio/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Sidenav from './components/Sidenav'
 import Main from './components/Main'
 import Work from './components/Work'
+import Projects from './components/Projects'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
 <Sidenav/>
 <Main/>
 <Work/>
+<Projects/>
     </>
   )
 }
---
+++ projectsReact/reactPortfolio/src/components/ProjectItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProjectItem() {
+  return (
+    <div>ProjectItem</div>
+  )
+}
+
+export default ProjectItem
