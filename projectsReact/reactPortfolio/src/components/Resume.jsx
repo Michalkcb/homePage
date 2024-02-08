ec0d8bf4e3637f486d3c7938746b31a748690d52
Michalkcb <michalkcb@gmail.com>
2024-02-08 14:18:30 +0100

    init resume component

---
+++ projectsReact/reactPortfolio/src/components/Resume.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">Contact</h1>
+      <form action="https://getform.io/f/b5ea2a6b-976a-457b-9188-0fc7a8788e14" method="POST" encType="multioart/form-data">
+        <div className="grid md:grid-cols-2 gap-4 w-full py-2" >
+    
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
