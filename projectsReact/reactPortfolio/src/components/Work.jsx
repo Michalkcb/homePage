64129c0765a21793173c86182f429e5528e31925
Michalkcb <michalkcb@gmail.com>
2024-02-08 20:29:16 +0100

    work updated

--- projectsReact/reactPortfolio/src/components/Work.jsx
+++ projectsReact/reactPortfolio/src/components/Work.jsx
@@ -7,18 +7,21 @@ const data = [
     duration: "1 year",
     title: "Front-end",
     details: "Working on this portfolio",
+    description: "",
   },
   {
     year: "2010",
     duration: "13 years",
     title:"Category Manager",
-    details:"Buying ,Sourcing (PL, EU, CN), Negotiations",
+    details:"Categories: toys, school, culture, sport, garden, camping, auto, diy, art. kitchen, art. for the table, home, textiles, furniture, lighting, intelligent home, pet food & accessories.",
+    description: `Buying, Sourcing (PL, EU, CN), Negotiations, Back catalog (SAP), Creating a trading strategy, Dynamizing commercial offers, Building promotional leaflets, Building promotional campaigns, Product development and on-boarding, Improvement of UX client experience, Profitability analysis (Microsoft BI), Creating store planograms, florplans (SpacePlanner), Close cooperation with marketing, VM department and logistics, Daily management of open orders between suppliers and customers, research on market trends and customer needs, (Sesta, Nielsen, GFK), Analysis of sales and market data, (Microsoft BI, google analytics),Implementation of solutions ensuring high profitability of products, Building marketing plans: weekly, monthly, quarter, annual, 3 years, Budget building and realization: weekly, monthly, quarter,annual, 3 years, Quality oversight and improvements in cooperation with Quality Department, Responsibility for key customers including sales and service related tasks, Co-creating the trade strategy of the sector in close cooperation with buyers, Managing the product range renovation project and guaranteeing the product range optimization process`,
   },
   {
     year: "2008",
     duration: "2 years",
     title: "IT, WebDev",
     details:"Web Development",
+    description: "Web Development and maintenance of the company's IT network of computers and peripheral devices. Development + administration of the company website www.albertech.pl. + SEO. Translation and preparation of advertising materials: graphics + editorial (brochures, catalogues, calendars, business cards). Contact with foreign and domestic suppliers",
   },
 ];
 
@@ -33,6 +36,7 @@ const Work = () => {
           title={item.title}
           duration={item.duration}
           details={item.details}
+          description={item.description}
         />
       ))}
     </div>
--- projectsReact/reactPortfolio/src/components/WorkItem.jsx
+++ projectsReact/reactPortfolio/src/components/WorkItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const WorkItem = ({ year, title, duration, details }) => {
+const WorkItem = ({ year, title, duration, details, description }) => {
   return (
     <ol className="flex flex-col md:flex-row relative border-l border-stone-200">
       <li className="mb-10 ml-4" >
@@ -11,6 +11,7 @@ const WorkItem = ({ year, title, duration, details }) => {
           <span className="my-1 text-sm font-normal leading-none text-stone-400" >{duration}</span>
         </p>
         <p className="my-2 text-base font-normal text-stone-500" >{details}</p>
+        <p className="my-2 text-base font-normal text-stone-500" >{description}</p>
       </li>
     </ol>
   );
