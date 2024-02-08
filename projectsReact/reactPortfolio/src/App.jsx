3124a0c2ba5f6a3ef7a6fc8222b3d67882a9d782
Michalkcb <michalkcb@gmail.com>
2024-02-08 11:22:56 +0100

    init contact component

--- projectsReact/reactPortfolio/src/App.jsx
+++ projectsReact/reactPortfolio/src/App.jsx
@@ -3,6 +3,7 @@ import Sidenav from './components/Sidenav'
 import Main from './components/Main'
 import Work from './components/Work'
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
 <Main/>
 <Work/>
 <Projects/>
+<Contact/>
     </>
   )
 }
