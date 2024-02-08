923d80f93bb3f2814ac1fb7b5a18b0b6c45b1b25
Michalkcb <michalkcb@gmail.com>
2024-02-08 22:08:50 +0100

    mahe project data component

--- projectsReact/reactPortfolio/src/components/ProjectItem.jsx
+++ projectsReact/reactPortfolio/src/components/ProjectItem.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const ProjectItem = ({ img, title }) => {
-    return (
-      <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-grey-400 rounded-lg group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]'>
-        <img src={img} alt="image" className='rounded-xl group-hover:opacity-10' />
-        <div className='hidden group-hover:block absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2' >
-          <h3 className='text-2xl font-bold text-white tracking-wider text-center' >
-            {title}
-          </h3>
-          <p className='pb-4 pt-2 text-white text-center' >React js</p>
-          <a href="/">
-            <p className='text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg' >More Info</p>
-          </a>
-        </div>
+const ProjectItem = ({ img, dsc, link, tech }) => {
+  return (
+    <div className="relative flex items-center h-[200px] w-[300px] overflow-hidden justify-center shadow-xl shadow-grey-400 rounded-lg group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
+      <img
+        src={img}
+        alt="image"
+        className="rounded-xl group-hover:opacity-10"
+      />
+      <div className="hidden group-hover:block absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2">
+        <h2>{tech}</h2>
+        <h3 className="text-2xl font-bold text-white tracking-wider text-center">
+          {dsc}
+        </h3>
+        <a href={link}>
+          <p className="pb-4 pt-2 text-white text-center">More Info</p>
+        </a>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-export default ProjectItem
+export default ProjectItem;
--- projectsReact/reactPortfolio/src/components/Projects.jsx
+++ projectsReact/reactPortfolio/src/components/Projects.jsx
@@ -1,80 +1,8 @@
 import React from 'react';
 import ProjectItem from './ProjectItem';
-import mbn from './../../../../assets/MBN.png';
-import faceGen from './../../../../assets/faceGenerator.png';
-import toast from './../../../../assets/toast.png';
-import ticTacToe from './../../../../assets/ttt.png';
-import typahead from './../../../../assets/typeahead.png';
-import stopwatch from './../../../../assets/stopwatch.png';
+import projectsData from './ProjectData';
 
-const projectsData = [
-  {tech:"html", link: '/projectsHtml/glowingIcons/index.html', imageSrc: 'https://michalkcb.github.io/homePage/assets/glowingItems.jpg', description: 'glowing_icons'},
-  {tech:"html", link: 'http://michalkcb.ct8.pl/new%20cruzeirosazuis/newTerminyPL.html',imageSrc: 'http://michalkcb.ct8.pl/pic/cruseiro.jpg',description: 'page_pic1'},
-  {tech:"html", link: 'http://michalkcb.ct8.pl/glowingDiv/index.html',imageSrc: 'http://michalkcb.ct8.pl/glowingDiv/glowingDivs.png',description: 'Glowing DIV'},
-  {tech:"html", link: '../old/index.html',imageSrc: 'http://michalkcb.ct8.pl/pic/old.png',description: 'old WD'},
-  {tech:"html", link: 'http://michalkcb.ct8.pl/page07/page07.html',imageSrc: 'http://michalkcb.ct8.pl/pic/page07.png',description: 'page_pic7'},
-  {tech:"html", link: 'http://michalkcb.ct8.pl/EuroChina/index.html',imageSrc: 'http://michalkcb.ct8.pl/pic/page02.png',description: 'page_pic2'},
-  {tech:"html", link: 'http://michalkcb.ct8.pl/page06/page06.html',imageSrc: 'http://michalkcb.ct8.pl/pic/page06.png',description: 'page_pic6'},
-  // {tech:"html", link: 'https://michalkcb.github.io/homePage/projectsHtml/semstorm/index.html',imageSrc: 'https://michalkcb.github.io/homePage/projectsHtml/semstorm/assets/semstorm.png',alt: 'semstorm'  }
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/sudoku.png', description: 'Sudoku (JS)', link: 'https://michalkcb.github.io/homePage/projectsJs/lvl3/sudoku/sudoku.html', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/MBN.png', description: 'Real Actual News Portal (JS)(API)', link: 'https://michalkcb.github.io/homePage/projectsJs/lvl2/MBNews/MBNews.html', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/toast.png', description: 'JS Toast Notifications', link: 'https://michalkcb.github.io/homePage/projectsJs/lvl1/toast_notification/toast.html', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/typeahead.png', description: 'JS typeahead', link: 'https://michalkcb.github.io/homePage/projectsJs/lvl2/simple_typehead/simple_typehead.html', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/stopwatch.png', description: 'JavaScript Stopwatch', link: 'https://michalkcb.github.io/homePage/projectsJs/lvl2/stoper/stopwatch.html', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/ttt.png', description: 'JavaScript Tic Tac Toe Game', link: 'https://michalkcb.github.io/homePage/projectsJs/lvl2/ticTacToe/ttt.html', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/projectsHtml/scrollAnimation/assets/scrollAnimate.png', description: 'JavaScript Animation on Scroll', link: './projectsHtml/scrollAnimation/index.html', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/Countries%20Lookup.png', description: 'Countries Lookup JSON BOOTWATCH', link: 'https://michalkcb.github.io/homePage/projectsJs/lvl2/typeahead/typeahead.html', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/faceGenerator.png', description: 'Face Generator JSON TailwindCSS', link: 'https://michalkcb.github.io/homePage/projectsJs/lvl2/FaceGenerator/dist/FaceGenerator.html', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/passwordGenerator.jpg', description: 'JS Password Generator', link: 'https://github.com/Michalkcb/myProjects/tree/master/ProjectsLevel2/passwordGenerator', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/infinitePageScroll.png', description: 'JS Infinite Page Scroller', link: 'https://github.com/Michalkcb/myProjects/blob/master/ProjectsLvl1/infiniteScroll/infiniteScroll.html', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/memoryGame.jpg', description: 'JS Memory Game', link: 'https://github.com/Michalkcb/myProjects/tree/master/ProjectsLevel2/theSimplestMemoryGame', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/fallingBall.jpg', description: 'JS Falling Ball Game', link: 'https://github.com/Michalkcb/myProjects/tree/master/ProjectsLevel2/fallingBall', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/dinoRun1.jpg', description: 'JS Dino Run on squares', link: 'https://github.com/Michalkcb/myProjects/tree/master/ProjectsLevel2/dinoGoogleGame', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/dinoRun2.jpg', description: 'JS Dino Run with graphic' , link: 'https://github.com/Michalkcb/myProjects/tree/master/ProjectsLevel2/chromeDinoClone',},
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/paint1.jpg', description: 'JS Paint in canvas', link: 'https://github.com/Michalkcb/myProjects/blob/master/ProjectsLvl1/anotherPaint/index.html', },
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/paint2.jpg', description: 'Another JS Paint in Canvas' , link: 'https://github.com/Michalkcb/myProjects/tree/master/ProjectsLvl1/yetAnotherPaintInCanvas',},
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/changeColorByBtn.jpg', description: 'Press button to change color' , link: 'https://github.com/Michalkcb/myProjects/tree/master/ProjectsLvl1/bgColorChangeByBtn',},
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/toDoApp.jpg', description: 'Simple to do app' , link: 'https://github.com/Michalkcb/myProjects/tree/master/ProjectsLvl1/toDoApp1',},
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/wordCounter.jpg', description: 'Counting words in text' , link: 'https://github.com/Michalkcb/myProjects/tree/master/ProjectsLvl1/wordsCounter',},
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/toDoApp.jpg', description: 'To do App witt Arry' , link: 'https://github.com/Michalkcb/myProjects/tree/master/ProjectsLvl1/toDoApp2Array',},
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/darkModeByBtn.jpg', description: 'Press btn to switch to dark mode' , link: 'https://github.com/Michalkcb/myProjects/tree/master/ProjectsLvl1/darkMode',},
-  { tech:"Java Script", imageSrc: 'https://michalkcb.github.io/homePage/assets/guessNumber.jpg', description: 'Game: guess number' , link: 'https://github.com/Michalkcb/myProjects/tree/master/ProjectsLvl1/guessNumber',},
-  { tech:"React",
-    link: 'https://michalkcb.github.io/homePage/projectsReact/toDoList/dist/index.html',
-    imageSrc: 'https://michalkcb.github.io/homePage/assets/todoReactVite.png',
-    description: 'react Todo List'
-  },
-  {tech:"React",
-    link: 'https://github.com/Michalkcb/myProjects/tree/master/react/react01',
-    imageSrc: 'https://michalkcb.github.io/homePage/assets/reactPage01.png',
-    description: 'react01'
-  },
-  {tech:"React",
-    link: 'https://github.com/Michalkcb/myProjects/tree/master/ProjectsLevel3/react_calculator/src',
-    imageSrc: 'https://michalkcb.github.io/homePage/assets/reactCalculator.png',
-    description: 'react Calculator'
-  },
-  { tech:"Tailwind",
-    link: 'https://michalkcb.github.io/homePage/tailwindCSS/tailwind01/dist/index.html',
-    imageSrc: 'https://michalkcb.github.io/homePage/tailwindCSS/tailwind01/assets/tailwind01.png',
-    description: 'pizzeria'
-  },
-  { tech:"Tailwind",
-    link: 'https://michalkcb.github.io/homePage/tailwindCSS/tailwind02/dist/index.html',
-    imageSrc: 'https://michalkcb.github.io/homePage/tailwindCSS/tailwind02/assets/aikido.png',
-    description: 'aikido'
-  },
-  {  tech:"Tailwind",
-    link: 'https://michalkcb.github.io/homePage/projectsJs/lvl2/FaceGenerator/dist/FaceGenerator.html',
-    imageSrc: 'https://michalkcb.github.io/homePage/assets/faceGenerator.png',
-    description: 'Face Generator'
-},
-  {  tech:"Python",
-    link: 'https://github.com/Michalkcb/Python/tree/main/wyzwaniePython/ticTacToe',
-    imageSrc: 'https://michalkcb.github.io/homePage/assets/python01.png',
-    description: 'Tic Tac Toe'
-  }
-];
+
 
 const Projects = () => {
   return (
@@ -82,16 +10,20 @@ const Projects = () => {
         <h1  className='text-4xl font-bold text-center text-[#001b5e]' >Projects</h1>
 
         <p className='text-center py-8' >
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste delectus facere suscipit quos illo! Ducimus totam recusandae aperiam architecto hic dolores, sed expedita veritatis animi at dignissimos vel natus consectetur?
+            Below you can find my latest projects done with different technologies like: HTML, CSS, Java Script, Reat, Tailwind, Python...
         </p>
-        <div className='grid sm:grid-cols-3 gap-12'>
-            <ProjectItem img={mbn} title='MBN App' />
-            <ProjectItem img={faceGen} title='Face Generator App' />
-            <ProjectItem img={toast} title='Toasts App' />
-            <ProjectItem img={ticTacToe} title='Tic Tac Toe App' />
-            <ProjectItem img={typahead} title='Typeahead App' />
-            <ProjectItem img={stopwatch} title='Stopwatch App' />
-        </div>
+        <div className="grid  lg:grid-cols-3 md:grid-cols-2 gap-12" >
+      {projectsData.map((item, idy)=>(
+        <ProjectItem 
+        key={idy}
+        tech={item.tech}
+        img={item.imageSrc}
+        link={item.link}
+        dsc={item.description}
+        />
+      ))}
+
+      </div>
     </div>
   )
 }
