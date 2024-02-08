626ff21d8ba93b7368f45b3301684f29f0d5cc65
Michalkcb <michalkcb@gmail.com>
2024-02-08 20:09:36 +0100

    form issuessolving

--- projectsReact/reactPortfolio/src/components/Contact.jsx
+++ projectsReact/reactPortfolio/src/components/Contact.jsx
@@ -7,24 +7,24 @@ const Contact = () => {
       <form action="https://getform.io/f/b5ea2a6b-976a-457b-9188-0fc7a8788e14" method="POST" encType="multioart/form-data">
         <div className="grid md:grid-cols-2 gap-4 w-full py-2" >
           <div className=" flex flex-col" >
-            <label className=" uppercase text-sm py-2" >Name</label>
-            <input className=" flex border-2 rounded-md p-3 border-gray-300" type="text" name="name" />
+            <label className=" uppercase text-sm py-2" for="name" >Name</label>
+            <input className=" flex border-2 rounded-md p-3 border-gray-300" type="text" name="name" id="name" autoComplete="off"/>
           </div>
           <div className=" flex flex-col">
-            <label className=" uppercase text-sm py-2">Phone Number</label>
-            <input className=" flex border-2 rounded-md p-3 border-gray-300" type="text" name="phone" />
+            <label className=" uppercase text-sm py-2" for="phone">Phone Number</label>
+            <input className=" flex border-2 rounded-md p-3 border-gray-300" type="text" name="phone" id="phone"autoComplete="off"/>
           </div>
           <div className=" flex flex-col col-span-2">
-            <label className=" uppercase text-sm py-2">Email</label>
-            <input className=" flex border-2 rounded-md p-3 border-gray-300" type="email" name="nemailame" />
+            <label className=" uppercase text-sm py-2" for="email">Email</label>
+            <input className=" flex border-2 rounded-md p-3 border-gray-300" type="email" name="nemailame" id="email"autoComplete="off"/>
           </div>
           <div className=" flex flex-col col-span-2">
-            <label className=" uppercase text-sm py-2">Subject</label>
-            <input className=" flex border-2 rounded-md p-3 border-gray-300" type="text" name="subject" />
+            <label className=" uppercase text-sm py-2" for="subject">Subject</label>
+            <input className=" flex border-2 rounded-md p-3 border-gray-300" type="text" name="subject" id="subject"/>
           </div>
           <div className=" flex flex-col col-span-2">
-            <label className=" uppercase text-sm py-2">Message</label>
-            <textarea className=" flex border-2 rounded-md p-3 border-gray-300" rows='10' name="message" />
+            <label className=" uppercase text-sm py-2" for="message">Message</label>
+            <textarea className=" flex border-2 rounded-md p-3 border-gray-300" rows='10' name="message" id="message"/>
           </div>
           <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg col-span-2" >
             Send Message
--- projectsReact/reactPortfolio/src/components/Resume.jsx
+++ projectsReact/reactPortfolio/src/components/Resume.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Resume = () => {
   return (
-    <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+    <div id="resume" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
         ABAUT ME
       </h1>
