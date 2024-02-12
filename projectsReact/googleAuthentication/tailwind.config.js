5ef2a2a1537cfb75e05ee3daf7b47749807562e0
Michalkcb <michalkcb@gmail.com>
2024-02-12 15:09:31 +0100

    init pages folder and signin page

--- projectsReact/googleAuthentication/tailwind.config.js
+++ projectsReact/googleAuthentication/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
