5e9235483252b7b117b6e7dcce7b9bf2444862bb
Michalkcb <michalkcb@gmail.com>
2024-02-12 18:56:26 +0100

    init authcontext

---
+++ projectsReact/googleAuthentication/src/context/AuthContext.js
@@ -0,0 +1,17 @@
+import { useContext, createContext } from "react";
+
+const AuthContext = createContext()
+
+export const AuthContextProvider = ({children}) => {
+    
+    return {
+        <AuthContext.Provider value={{}}>
+            {children}
+        </AuthContext.Provider>
+
+    }
+}
+
+export const UserAuth =() => {
+    return useContext(AuthContext)
+}
