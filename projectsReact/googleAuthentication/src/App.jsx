26cf422671a367928d4885b41ade5da44f9f5cf7
Michalkcb <michalkcb@gmail.com>
2024-02-12 15:51:08 +0100

    init app

--- projectsReact/googleAuthentication/src/App.jsx
+++ projectsReact/googleAuthentication/src/App.jsx
@@ -1,10 +1,23 @@
+import React from "react"
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Account from "./pages/Account"
+import Home from "./pages/Home"
+import Signin from "./pages/Signin"
+ 
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={Home}/>
+        <Route path="/" element={Signin}/>
+        <Route path="/" element={Account}/>
+      </Routes>
     </>
   )
 }
