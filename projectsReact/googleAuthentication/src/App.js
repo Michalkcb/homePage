ac59a525d859c11e7f7c2c78416876082f8f57c6
Michalkcb <michalkcb@gmail.com>
2024-02-13 20:53:37 +0100

    import and ser authcontext provider in app.js

--- projectsReact/googleAuthentication/src/App.js
+++ projectsReact/googleAuthentication/src/App.js
@@ -4,19 +4,25 @@ import Navbar from "./components/Navbar"
 import Account from "./pages/Account"
 import Home from "./pages/Home"
 import Signin from "./pages/Signin"
+import { AuthContextProvider } from "./context/AuthContext"
  
 
 
 function App() {
   return (
-    <>
+    <div>
+      <AuthContextProvider>
+
       <Navbar/>
       <Routes>
         <Route path="/" element={Home}/>
         <Route path="/" element={Signin}/>
         <Route path="/" element={Account}/>
       </Routes>
-    </>
+
+      </AuthContextProvider>
+
+    </div>
   )
 }
 
