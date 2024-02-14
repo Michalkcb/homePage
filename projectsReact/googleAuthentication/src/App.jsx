aa885669a736664516b3233efce53b01bd777b16
Michalkcb <michalkcb@gmail.com>
2024-02-14 18:36:58 +0100

    project without errors

---
+++ projectsReact/googleAuthentication/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { AuthContextProvider } from './context/AuthContext';
+import Account from './pages/Account';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+
+function App() {
+  return (
+    <div>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route/>
+        </Routes>
+      </AuthContextProvider>
+    </div>
+  );
+}
+
+export default App;
--- projectsReact/googleAuthentication/src/firebase.js
+++ projectsReact/googleAuthentication/src/firebase.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCtdcGos8jtphMskeQQh88vcfKWT1IDbqA",
-  authDomain: "auth-project-2825c.firebaseapp.com",
-  projectId: "auth-project-2825c",
-  storageBucket: "auth-project-2825c.appspot.com",
-  messagingSenderId: "3644925167",
-  appId: "1:3644925167:web:6023bd75bbd88b267e271a"
+  apiKey: "YOUR FIREBASE API KEY",
+  authDomain: "YOUR FIREBASE AUTHDOMAIN",
+  projectId: "YOUR FIREBASE PROJECTID",
+  storageBucket: "YOUR FIREBASE STORAGE BUCKET",
+  messagingSenderId: "YOUR FIREBASE MESSAGESENDER ID",
+  appId: "YOUR FIREBASE APPID"
 };
 
 // Initialize Firebase
