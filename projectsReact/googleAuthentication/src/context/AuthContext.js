6025f83832624d556c44d59c6847c9829480c937
Michalkcb <michalkcb@gmail.com>
2024-02-14 15:39:58 +0100

    add google sign in

--- projectsReact/googleAuthentication/src/context/AuthContext.js
+++ projectsReact/googleAuthentication/src/context/AuthContext.js
@@ -4,14 +4,22 @@ import {
   signInWithPopup,
   signInWithRedirect,
   signOut,
-  onAuthStateChanged
+  onAuthStateChanged,
 } from "firebase/auth";
 import auth from "../firebase";
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+    const googleSignIn = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider);
+    }
+  return(
+  <AuthContext.Provider value={{googleSignIn}}>
+    {children}
+    </AuthContext.Provider>
+  )
 };
 
 export const UserAuth = () => {
