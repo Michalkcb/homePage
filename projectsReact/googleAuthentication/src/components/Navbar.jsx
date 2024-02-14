9de09e1cccb6c30d4fc9ec542553cdddea94a417
Michalkcb <michalkcb@gmail.com>
2024-02-14 20:43:41 +0100

    add sign out and handle signout in navbar

--- projectsReact/googleAuthentication/src/components/Navbar.jsx
+++ projectsReact/googleAuthentication/src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import UserAuth from '../context/AuthContext'
 
 
 const Navbar = () => {
+
+  const {user, logOut} = UserAuth();
+
+  const handleSignOut = async () => {
+    try {
+      await logOut()
+    }catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className=' flex justify-between bg-gray-200 w-full p-4'>
         <h1 className=' text-center text-2xl font-bold'>
             Google Auth
         </h1>
-        <Link to='./signin'>Sign in</Link>
+        {user?.displayName ? <button onClick={handleSignOut}>Logout</button> : <Link to='./signin'>Sign in</Link> }
     </div>
   )
 }
