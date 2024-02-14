c1ce6ad8e56c270570d0bbdad327e2fae7b654f6
Michalkcb <michalkcb@gmail.com>
2024-02-14 16:24:06 +0100

    managing errors

--- projectsReact/googleAuthentication/src/pages/Signin.jsx
+++ projectsReact/googleAuthentication/src/pages/Signin.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import GoogleButton from 'react-google-button'
+import { UserAuth } from '../context/AuthContext'
 
 const Signin = () => {
+  const googleSignIn = UserAuth();
+
+  const handleGoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
         <h1 className=' text-center text-3xl font-bold py-8'>Sign in</h1>
-        <div  className='max-w-[240px] m-auto py-4'> <GoogleButton/> </div>
+        <div  className='max-w-[240px] m-auto py-4'> 
+        <GoogleButton/> 
+        </div>
     </div>
   )
 }
