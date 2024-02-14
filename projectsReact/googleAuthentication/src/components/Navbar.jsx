2fc2c6588cd55ddad0ed3710357e8d6bb8e4a230
Michalkcb <michalkcb@gmail.com>
2024-02-14 21:54:23 +0100

    stylinf signin

--- projectsReact/googleAuthentication/src/components/Navbar.jsx
+++ projectsReact/googleAuthentication/src/components/Navbar.jsx
@@ -1,28 +1,36 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {UserAuth} from '../context/AuthContext'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
-
-  const {user, logOut} = UserAuth();
+  const { user, logOut } = UserAuth();
 
   const handleSignOut = async () => {
     try {
-      await logOut()
-    }catch (error) {
-      console.log(error)
+      await logOut();
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
 
   return (
-    <div className=' flex justify-between bg-gray-200 w-full p-4'>
-        <h1 className=' text-center text-2xl font-bold'>
-            Google Auth
-        </h1>
-        {user?.displayName ? <button onClick={handleSignOut}>Logout</button> : (<Link to='./signin'>Sign in</Link>) }
+    <div className=" flex justify-between bg-gray-200 w-full p-4">
+      <div className="h-12 w-auto m-2 flex items-center justify-center">
+          <img
+            className="filter invert h-12 w-14"
+            src="https://michalkcb.github.io/homePage/assets/MB_logo.png"
+            alt="My Logo"
+          />
+      <h1 className=" text-center text-2xl font-bold ml-8">Google Auth</h1>
+      </div>
+      {user?.displayName ? (
+        <button onClick={handleSignOut}>
+          Logout</button>
+      ) : (
+          <Link to="./signin" className=" text-center font-bold ml-8 flex items-center"><p className=" text-center">Sign in</p></Link>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- projectsReact/googleAuthentication/src/pages/Home.jsx
+++ projectsReact/googleAuthentication/src/pages/Home.jsx
@@ -4,8 +4,6 @@ const Home = () => {
   return (
     <div>
       <h1 className='text-center text-3xl font-bold py-8'>Home Page</h1>
-      <h1 className=' '>Home Page</h1>
-
     </div>
   )
 }
