9d8d590b5cb850bca100ce14d804f16f54f2e629
Michalkcb <michalkcb@gmail.com>
2024-02-14 18:58:57 +0100

    reinit tailwind but its not working

--- projectsReact/googleAuthentication/src/pages/Signin.jsx
+++ projectsReact/googleAuthentication/src/pages/Signin.jsx
@@ -3,7 +3,7 @@ import GoogleButton from 'react-google-button'
 import { UserAuth } from '../context/AuthContext'
 
 const Signin = () => {
-  const googleSignIn = UserAuth();
+  const { googleSignIn } = UserAuth();
 
   const handleGoogleSignIn = async () => {
     try {
@@ -15,12 +15,12 @@ const Signin = () => {
 
   return (
     <div>
-        <h1 className=' text-center text-3xl font-bold py-8'>Sign in</h1>
-        <div  className='max-w-[240px] m-auto py-4'> 
-        <GoogleButton/> 
-        </div>
+      <h1 className='text-center text-3xl font-bold py-8'>Sign in</h1>
+      <div className='max-w-[240px] m-auto py-4'>
+        <GoogleButton onClick={handleGoogleSignIn} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
--- projectsReact/googleAuthentication/tailwind.config.js
+++ projectsReact/googleAuthentication/tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/**/*.{html,js}"
-  ],
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
