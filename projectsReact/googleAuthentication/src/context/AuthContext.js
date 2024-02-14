a4fc2753f039eec967fb4dcf1b1beaf800aab37b
Michalkcb <michalkcb@gmail.com>
2024-02-14 15:31:43 +0100

    import auth from firebase to AythContext

--- projectsReact/googleAuthentication/src/context/AuthContext.js
+++ projectsReact/googleAuthentication/src/context/AuthContext.js
@@ -1,16 +1,19 @@
 import { useContext, createContext } from "react";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithRedirect,
+  signOut,
+  onAuthStateChanged
+} from "firebase/auth";
+import auth from "../firebase";
 
 const AuthContext = createContext();
 
-export const AuthContextProvider = ({ children }) => {    
-    return (
-        
-        <AuthContext.Provider value={{}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+export const AuthContextProvider = ({ children }) => {
+  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+};
 
-export const UserAuth =() => {
-    return useContext(AuthContext)
-}
+export const UserAuth = () => {
+  return useContext(AuthContext);
+};
