abc59709cbebd3334f06808b0db7adf4275e9c3f
Michalkcb <michalkcb@gmail.com>
2024-02-16 17:36:15 +0100

    set tailwind.config

--- projectsReact/crypto/postcss.config.js
+++ projectsReact/crypto/postcss.config.js
@@ -1,6 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 }
