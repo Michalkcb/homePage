103dffb37c5fe83d05dde00845e0bd694312cda8
Michalkcb <michalkcb@gmail.com>
2024-02-17 14:41:42 +0100

    set theme colors in tailwind config

--- projectsReact/crypto/tailwind.config.js
+++ projectsReact/crypto/tailwind.config.js
@@ -9,7 +9,15 @@ export default {
   theme: {
     extend: {
       backgroundColor: {
-        primary: 'var(--color-bg-primary)'
+        primary: 'var(--color-bg-primary)',
+        secondary: 'var(--color-bg-secondary)',
+        button: 'var(--color-bg-button)',
+      },
+      textColor: {
+        accent:  'var(--color-text-accent)',
+        primary:  'var(--color-text-primary)',
+        secondary:  'var(--color-text-secondary)',
+        btnText:  'var(--color-bg-secondary)',
       }
     },
   },
