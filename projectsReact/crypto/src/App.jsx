d6f3b754b7df4de42b25fe6280b8d9ba79fd052c
Michalkcb <michalkcb@gmail.com>
2024-02-17 13:58:22 +0100

    ready empty project

--- projectsReact/crypto/src/App.jsx
+++ projectsReact/crypto/src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
+      <h1>Hello World</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
