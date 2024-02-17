657376424aecbcd29bfe482a1812eab81f49d64a
Michalkcb <michalkcb@gmail.com>
2024-02-17 14:26:27 +0100

    add dark mode to tailwind config, add custom colors in index.css

--- projectsReact/crypto/tailwind.config.js
+++ projectsReact/crypto/tailwind.config.js
@@ -3,9 +3,15 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: 'var(--color-bg-primary)'
+      }
+    },
   },
   plugins: [],
 }
