518c28fa8619fb59860e31a70ce984ae1b2c1460
Michalkcb <michalkcb@gmail.com>
2024-02-17 14:20:10 +0100

    tailwind works

--- projectsReact/crypto/src/App.jsx
+++ projectsReact/crypto/src/App.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+          <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   );
 }
