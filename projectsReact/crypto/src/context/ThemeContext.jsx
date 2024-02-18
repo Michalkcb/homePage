a4f3cece061d2c0ba91d0812d67f1d55e47691a6
Michalkcb <michalkcb@gmail.com>
2024-02-18 17:14:22 +0100

    theme context done

--- projectsReact/crypto/src/context/ThemeContext.jsx
+++ projectsReact/crypto/src/context/ThemeContext.jsx
@@ -1,9 +1,17 @@
 import React, {useState, useEffect, createContext} from 'react'
 
-const ThemeContext = () => {
-  return (
-    <div>ThemeContext</div>
-  )
+const getInitialTheme = () => {
+    if (typeof window != 'undefined' && window.localStorage){
+        const storedPrefs  =window.localStorage.getItem('lolor-theme')
+        if (typeof storedPrefs === 'string'){
+            return storedPrefs
+        }
+        const userMedia  =window.matchMedia('prefers-sheme: dark')
+        if (userMedia.matches){
+            return 'dark'
+        }
+    }
+  return 'light'
 }
 
 export default ThemeContext
