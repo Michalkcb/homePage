48bce6d4da25ab565bb317ef6e77f97da9127534
Michalkcb <michalkcb@gmail.com>
2024-02-19 20:37:21 +0100

    init import NMavbar in App

--- projectsReact/crypto/src/App.jsx
+++ projectsReact/crypto/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { ThemeProvider } from "./context/ThemeContext";
+import Navbar from "./components/Navbar";
 
 function App() {
   return <ThemeProvider>
+      <Navbar></Navbar>
     </ThemeProvider>;
 }
 
