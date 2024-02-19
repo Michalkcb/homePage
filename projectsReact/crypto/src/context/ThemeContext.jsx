f949113b8b72cb52b67bc4e675e5b8e4790831f5
Michalkcb <michalkcb@gmail.com>
2024-02-19 20:08:21 +0100

    working on Theme provider

--- projectsReact/crypto/src/context/ThemeContext.jsx
+++ projectsReact/crypto/src/context/ThemeContext.jsx
@@ -14,4 +14,31 @@ const getInitialTheme = () => {
   return 'light'
 }
 
-export default ThemeContext
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({initialTheme, children}) => {
+    const [theme, setTheme] = useState(getInitialTheme)
+
+    const rawSetTheme = (theme) => {
+        const root = window.document.documentElement;
+        const isDark = theme === 'dark';
+
+        root.classList.remove(isDark ? 'light' : 'dark')
+        root.classList.add(theme)
+
+        localStorage.setItem('color-theme', theme)
+    }
+    if (initialTheme){
+        rawSetTheme(initialTheme)
+    }
+
+    useEffect(()=>{
+        rawSetTheme(theme)
+    }, [theme])
+
+    return (
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
