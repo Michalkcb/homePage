ee2dc813a89b3ebde0043a3bbf5547c9226541ff
Michalkcb <michalkcb@gmail.com>
2024-02-19 20:44:49 +0100

    Change project name to Crypto Hub

--- projectsReact/crypto/src/components/Navbar.jsx
+++ projectsReact/crypto/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <Link to='/'>
+            <h1>MB CryptoHub</h1>
+        </Link>
+    </div>
   )
 }
 
