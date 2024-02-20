58a1525d430c3957ee73a25216c406095e781640
Michalkcb <michalkcb@gmail.com>
2024-02-20 19:08:00 +0100

    add logo, icon

--- projectsReact/crypto/src/components/Navbar.jsx
+++ projectsReact/crypto/src/components/Navbar.jsx
@@ -5,8 +5,11 @@ import ThemeToggle from './ThemeToggle'
 const Navbar = () => {
   return (
     <div>
-        <Link to='/'>
-            <h1 className='border font-bold'>MB CryptoHub</h1>
+        <Link to='/' className='flex items-center'>
+        <img src="https://michalkcb.github.io/homePage/assets/MB_logo.png" alt="" className='filter invert max-h-[40px] p-2'/>
+            <h1 className=' font-bold '> 
+           
+              CryptoHub</h1>
         </Link>
         <div>
             <ThemeToggle/>
