993f0c5a0a22b4f65c32c1da281c1c168951c0a4
Michalkcb <michalkcb@gmail.com>
2024-02-20 19:24:01 +0100

    add mobile menu

--- projectsReact/crypto/src/components/Navbar.jsx
+++ projectsReact/crypto/src/components/Navbar.jsx
@@ -1,29 +1,48 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import ThemeToggle from './ThemeToggle'
-import {AiOutlirMenu} from 'react-icons/ai'
+import React from "react";
+import { Link } from "react-router-dom";
+import ThemeToggle from "./ThemeToggle";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   return (
     <div>
-        <Link to='/' className='flex items-center'>
-        <img src="https://michalkcb.github.io/homePage/assets/MB_logo.png" alt="" className='filter invert max-h-[40px] p-2'/>
-            <h1 className=' font-bold '> 
-           
-              CryptoHub</h1>
-        </Link>
+      <Link to="/" className="flex items-center">
+        <img
+          src="https://michalkcb.github.io/homePage/assets/MB_logo.png"
+          alt=""
+          className="filter invert max-h-[40px] p-2"
+        />
+        <h1 className=" font-bold ">CryptoHub</h1>
+      </Link>
+      <div>
+        <ThemeToggle />
+      </div>
+      <div>
+        <Link to="./signin">Sign In</Link>
+        <Link to="./signup">Sign Up</Link>
+      </div>
+      <div>
+        <AiOutlineMenu></AiOutlineMenu>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">Account</Link>
+          </li>
+          <li>
+            <ThemeToggle></ThemeToggle>
+          </li>
+        </ul>
         <div>
-            <ThemeToggle/>
+          <Link to="./signin"> <button>Sign In</button> </Link>
+          <Link to="./signup"> <button>Sign Up</button> </Link>
         </div>
-        <div>
-         <Link to='./signin'>Sign In</Link>
-         <Link to='./signup'>Sign Up</Link>
-        </div>
-        <div>
-          <AiOutlirMenu></AiOutlirMenu>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
