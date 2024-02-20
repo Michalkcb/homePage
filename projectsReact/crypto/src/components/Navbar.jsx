015090e740c0085a04e0578dcf1f4c377f263b03
Michalkcb <michalkcb@gmail.com>
2024-02-20 20:11:32 +0100

    nav done

--- projectsReact/crypto/src/components/Navbar.jsx
+++ projectsReact/crypto/src/components/Navbar.jsx
@@ -4,11 +4,11 @@ import ThemeToggle from "./ThemeToggle";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
 
   const handleNav = () => {
-    setNav(!nav)
-  }
+    setNav(!nav);
+  };
 
   return (
     <div className=" rounded-div flex items-center justify-between h-20 font-bold">
@@ -24,15 +24,28 @@ const Navbar = () => {
         <ThemeToggle />
       </div>
       <div className=" hidden md:block">
-        <Link to="./signin" className="p-4 hover:text-accent">Sign In</Link>
-        <Link to="./signup" className=" bg-button text-btnText px-5 py-2 rounded-2xl shadow-lg hover:shadow-2xl">Sign Up</Link>
+        <Link to="./signin" className="p-4 hover:text-accent">
+          Sign In
+        </Link>
+        <Link
+          to="./signup"
+          className=" bg-button text-btnText px-5 py-2 rounded-2xl shadow-lg hover:shadow-2xl"
+        >
+          Sign Up
+        </Link>
       </div>
       <div onClick={handleNav} className="block mb:hidden cursor-pointer z-10">
-        {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
 
       {/* mobile menu */}
-      <div className=" md:hidden fixed left-0 top-20 flex flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-300 z-10">
+      <div
+        className={
+          nav
+            ? " md:hidden fixed left-0 top-20 flex flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-300 z-10"
+            : " fixed left-[-100%] top-20 h-[90%] flex flex-col items-center justify-between ease-in duration-300"
+        }
+      >
         <ul className="w-full p-4">
           <li className="border py-6 ">
             <Link to="/">Home</Link>
@@ -45,8 +58,18 @@ const Navbar = () => {
           </li>
         </ul>
         <div className="flex flex-col w-full p-4">
-          <Link to="./signin"> <button className="w-full my-2 p-3 bg-primary text-primary border border-secondary rounded-2xl shadow-xl">Sign In</button> </Link>
-          <Link to="./signup"> <button className=" w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl">Sign Up</button> </Link>
+          <Link to="./signin">
+            {" "}
+            <button className="w-full my-2 p-3 bg-primary text-primary border border-secondary rounded-2xl shadow-xl">
+              Sign In
+            </button>{" "}
+          </Link>
+          <Link to="./signup">
+            {" "}
+            <button className=" w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl">
+              Sign Up
+            </button>{" "}
+          </Link>
         </div>
       </div>
     </div>
