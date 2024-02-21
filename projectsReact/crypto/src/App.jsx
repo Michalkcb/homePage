10742f2c31fe6ca4e28ec348603f6c84c0f85747
Michalkcb <michalkcb@gmail.com>
2024-02-21 18:55:26 +0100

    add different routes in app

--- projectsReact/crypto/src/App.jsx
+++ projectsReact/crypto/src/App.jsx
@@ -3,15 +3,18 @@ import { ThemeProvider } from "./context/ThemeContext";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
+import SignUp from "./routes/SignUp";
+import SignIn from "./routes/SignIn";
+import Account from "./routes/Account";
 
 function App() {
   return <ThemeProvider>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/" element={<Home/>}/>
+        <Route path="./signin" element={<SignIn/>}/>
+        <Route path="./signup" element={<SignUp/>}/>
+        <Route path="/account" element={<Account/>}/>
       </Routes>
     </ThemeProvider>;
 }
