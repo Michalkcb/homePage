c6ffdfe153924938248caf4cd20d497b0fc599e7
Michalkcb <michalkcb@gmail.com>
2024-02-21 18:48:48 +0100

    import coin search in home

--- projectsReact/crypto/src/routes/Home.jsx
+++ projectsReact/crypto/src/routes/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import CoinSearch from '../components/CoinSearch'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <CoinSearch/>
+    </div>
   )
 }
 
