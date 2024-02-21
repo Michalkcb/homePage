ac89527976839727b5fc6159e1b653952327ea70
Michalkcb <michalkcb@gmail.com>
2024-02-21 18:51:10 +0100

    make Coin search route in app

--- projectsReact/crypto/src/App.jsx
+++ projectsReact/crypto/src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { ThemeProvider } from "./context/ThemeContext";
 import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
 
 function App() {
   return <ThemeProvider>
-      <Navbar></Navbar>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </ThemeProvider>;
 }
 
