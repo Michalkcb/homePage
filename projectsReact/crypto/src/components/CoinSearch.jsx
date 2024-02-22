f6143d7e9d5db4339b2186719dbb7c2d7fd97fcd
Michalkcb <michalkcb@gmail.com>
2024-02-22 18:39:27 +0100

    add coin table in search coin

--- projectsReact/crypto/src/components/CoinSearch.jsx
+++ projectsReact/crypto/src/components/CoinSearch.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
+import {AiOutlineStar} from 'react-icons/ai';
 
-const CoinSearch = ({coins}) => {
+const CoinSearch = ({ coins }) => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div>
+        <h1>Search crypto</h1>
+        <form>
+          <input type="text" placeholder="Search a coin" />
+        </form>
+      </div>
 
-export default CoinSearch
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>#</th>
+            <th>Coin</th>
+            <th></th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>24h volume</th>
+            <th>Mkt</th>
+            <th>Last 7 Days</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin)=>(
+            <tr>
+              <td> <AiOutlineStar/> </td>
+              <td>{coin.market_cap_rank}</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CoinSearch;
