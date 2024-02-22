1f4f27809f84fe9163ef488c37325ee5410411a5
Michalkcb <michalkcb@gmail.com>
2024-02-22 19:36:41 +0100

    add input onchange

--- projectsReact/crypto/src/components/CoinSearch.jsx
+++ projectsReact/crypto/src/components/CoinSearch.jsx
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 import CoinItem from "./CoinItem";
 
 const CoinSearch = ({ coins }) => {
-  const [searchText, setSearchText] = useState('')
+  const [searchText, setSearchText] = useState("");
 
   return (
     <div>
       <div>
         <h1>Search crypto</h1>
         <form>
-          <input type="text" placeholder="Search a coin" />
+          <input onChange={(e)=>setSearchText(e.target.value)} type="text" placeholder="Search a coin" />
         </form>
       </div>
 
@@ -29,7 +29,19 @@ const CoinSearch = ({ coins }) => {
           </tr>
         </thead>
         <tbody>
-          {coins.map((coin) => (
+          {coins
+            .filter((value) => {
+              if ((searchText === "")) {
+                return value;
+              } else if (
+                value.name
+                  .toLowerCase()
+                  .includes(searchText.toLocaleLowerCase())
+              ) {
+                return value;
+              }
+            })
+            .map((coin) => (
               <CoinItem key={coin.id} coin={coin} />
             ))}
         </tbody>
