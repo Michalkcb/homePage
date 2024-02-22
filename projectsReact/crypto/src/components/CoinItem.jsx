146311830ce281cbae1ba4396a7562f29367c7a6
Michalkcb <michalkcb@gmail.com>
2024-02-22 19:25:56 +0100

    init coin item component

---
+++ projectsReact/crypto/src/components/CoinItem.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { AiOutlineStar } from "react-icons/ai";
+import { Sparklines, SparklinesLine  } from 'react-sparklines';
+
+const CoinItem = ({coin}) => {
+  return (
+      
+            <tr>
+              <td>
+                {" "}
+                <AiOutlineStar />{" "}
+              </td>
+              <td>{coin.market_cap_rank}</td>
+              <td>
+                <div>
+                  <img src={coin.image} alt={coin.id} />
+                  <p>{coin.name}</p>
+                </div>
+              </td>
+              <td>{coin.symbol}</td>
+              <td>{coin.current_price}</td>
+              <td>{coin.price_change_percentage_24h}</td>
+              <td>{coin.total_volume}</td>
+              <td>{coin.market_cap}</td>
+              <td>
+                <Sparklines data={coin.sparkline_in_7d.price}>
+                  <SparklinesLine color='blue'/>
+                </Sparklines>
+                </td>
+            </tr>
+
+  )
+}
+
+export default CoinItem
--- projectsReact/crypto/src/components/CoinSearch.jsx
+++ projectsReact/crypto/src/components/CoinSearch.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import { AiOutlineStar } from "react-icons/ai";
-import { Sparklines, SparklinesLine  } from 'react-sparklines';
+import React, { useState } from "react";
+
+import CoinItem from "./CoinItem";
 
 const CoinSearch = ({ coins }) => {
+  const [searchText, setSearchText] = useState('')
+
   return (
     <div>
       <div>
@@ -28,30 +30,8 @@ const CoinSearch = ({ coins }) => {
         </thead>
         <tbody>
           {coins.map((coin) => (
-            <tr>
-              <td>
-                {" "}
-                <AiOutlineStar />{" "}
-              </td>
-              <td>{coin.market_cap_rank}</td>
-              <td>
-                <div>
-                  <img src={coin.image} alt={coin.id} />
-                  <p>{coin.name}</p>
-                </div>
-              </td>
-              <td>{coin.symbol}</td>
-              <td>{coin.current_price}</td>
-              <td>{coin.price_change_percentage_24h}</td>
-              <td>{coin.total_volume}</td>
-              <td>{coin.market_cap}</td>
-              <td>
-                <Sparklines data={coin.sparkline_in_7d.price}>
-                  <SparklinesLine color='blue'/>
-                </Sparklines>
-                </td>
-            </tr>
-          ))}
+              <CoinItem key={coin.id} coin={coin} />
+            ))}
         </tbody>
       </table>
     </div>
