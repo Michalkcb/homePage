bc6c24ee0761bd059d2212a7389c996f4a35ce0b
Michalkcb <michalkcb@gmail.com>
2024-02-22 19:42:27 +0100

    add input style

--- projectsReact/crypto/src/components/CoinSearch.jsx
+++ projectsReact/crypto/src/components/CoinSearch.jsx
@@ -10,7 +10,8 @@ const CoinSearch = ({ coins }) => {
       <div className="flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
         <h1 className="text-2xl font-bold my-2">Search crypto</h1>
         <form>
-          <input onChange={(e)=>setSearchText(e.target.value)} type="text" placeholder="Search a coin" />
+          <input onChange={(e)=>setSearchText(e.target.value)} 
+          className=" w-full bg-primary border border-imput px-4 py-2 rounded-2xl shadow-xl" type="text" placeholder="Search a coin" />
         </form>
       </div>
 
