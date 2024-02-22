f568bcc9cfe3316d72c0c0f9b6dbc5288f5dd5ed
Michalkcb <michalkcb@gmail.com>
2024-02-22 19:40:04 +0100

    add h1 style

--- projectsReact/crypto/src/components/CoinSearch.jsx
+++ projectsReact/crypto/src/components/CoinSearch.jsx
@@ -8,7 +8,7 @@ const CoinSearch = ({ coins }) => {
   return (
     <div className=" rounded-div my-24">
       <div className="flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
-        <h1>Search crypto</h1>
+        <h1 className="text-2xl font-bold my-2">Search crypto</h1>
         <form>
           <input onChange={(e)=>setSearchText(e.target.value)} type="text" placeholder="Search a coin" />
         </form>
