a90f64cd1077f221e7e1a61e0a1139c524344627
Michalkcb <michalkcb@gmail.com>
2024-02-23 16:39:59 +0100

    add key in trending

--- projectsReact/crypto/src/App.jsx
+++ projectsReact/crypto/src/App.jsx
@@ -9,25 +9,33 @@ import Account from "./routes/Account";
 import axios from "axios";
 
 function App() {
-  const [coins, setCoins] = useState([])
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=PLN&order=market_cap_desc&per_page=10&page=1&sparkline=true&locale=pl&precision=2'
+  const [coins, setCoins] = useState([]);
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=PLN&order=market_cap_desc&per_page=10&page=1&sparkline=true&locale=pl&precision=2";
 
-  useEffect(()=> {
-    axios.get(url).then((response)=> {
-      setCoins(response.data)
-      console.log(response.data)
-    })
-  }, [url])
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+      console.log(response.data);
+    });
+  }, [url]);
 
-  return <ThemeProvider>
-      <Navbar/>
+  return (
+    <ThemeProvider>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home coins={coins}/>}/>
-        <Route path="./signin" element={<SignIn/>}/>
-        <Route path="./signup" element={<SignUp/>}/>
-        <Route path="/account" element={<Account/>}/>
+        <Route path="/" element={<Home coins={coins} />} />
+        <Route path="./signin" element={<SignIn />} />
+        <Route path="./signup" element={<SignUp />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/coin/:coinId" element={<CoinPage />}>
+          <Route path=":coinId">
+
+          </Route>
+        </Route>
       </Routes>
-    </ThemeProvider>;
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- projectsReact/crypto/src/components/Trending.jsx
+++ projectsReact/crypto/src/components/Trending.jsx
@@ -16,8 +16,8 @@ const Trending = () => {
     <div className="rounded-div my-12 py-8 text-primary" >
       <h1 className="text-2xl font-bold py-4">Trending Coins</h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {trending.map((coin) => (
-          <div className="rounded-div flex justify-between p-4 hover:scale-105 ease-in-out duration-300">
+        {trending.map((coin, index) => (
+          <div key={index} className="rounded-div flex justify-between p-4 hover:scale-105 ease-in-out duration-300">
             <div className="flex w-full items-center justify-between">
               <div className="flex">
                 <img className="mr-4 rounded-full" src={coin.item.small} alt="/" />
