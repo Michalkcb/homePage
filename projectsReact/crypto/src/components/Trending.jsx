3352099fa0c55abf529b71807c9134669453e2fe
Michalkcb <michalkcb@gmail.com>
2024-02-23 16:13:47 +0100

    trending.map change ()=>{} to ()=>()

--- projectsReact/crypto/src/components/Trending.jsx
+++ projectsReact/crypto/src/components/Trending.jsx
@@ -8,6 +8,7 @@ const Trending = () => {
   useEffect(() => {
     axios.get(url).then((response) => {
       setTrending(response.data.coins);
+      console.log(response.data.coins)
     });
   }, []);
 
@@ -15,7 +16,7 @@ const Trending = () => {
     <div>
       <h1>Trending Coins</h1>
       <div>
-        {trending.map((coin) => {
+        {trending.map((coin) => (
           <div>
             <div>
               <div>
@@ -25,9 +26,17 @@ const Trending = () => {
                   <p>{coin.item.symbol}</p>
                 </div>
               </div>
+              <div className="flex items-center">
+                <img
+                  className="w-4 mr-2"
+                  src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+                  alt="/"
+                />
+                <p>{coin.item.price_btc.toFixed(7)}</p>
+              </div>
             </div>
-          </div>;
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
