7314b4d257eb753bc2d4dd8e0c297abadf977672
Michalkcb <michalkcb@gmail.com>
2024-02-23 16:56:32 +0100

    add bitcoin url

--- projectsReact/crypto/src/routes/CoinPage.jsx
+++ projectsReact/crypto/src/routes/CoinPage.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from 'react'
-import axios  from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const CoinPage = () => {
-  const [coin, setCoin] = useState({})
-  const url= "https://api.coingecko.com/api/v3/coins/${params.coinId}?localization=false&sparkline=true"
-  return (
-    <div>CoinPage</div>
-  )
-}
+  const [coin, setCoin] = useState({});
+  const url =
+    "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&sparkline=true";
 
-export default CoinPage
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoin(response.data);
+      console.log(response.data);
+    });
+  }, [url]);
+
+  return <div>CoinPage</div>;
+};
+
+export default CoinPage;
