0436b912c884a7c99d7915595584b6efd950f4e3
Michalkcb <michalkcb@gmail.com>
2024-02-23 16:35:40 +0100

    styling trending icons

--- projectsReact/crypto/src/components/Trending.jsx
+++ projectsReact/crypto/src/components/Trending.jsx
@@ -15,14 +15,14 @@ const Trending = () => {
   return (
     <div className="rounded-div my-12 py-8 text-primary" >
       <h1 className="text-2xl font-bold py-4">Trending Coins</h1>
-      <div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
         {trending.map((coin) => (
           <div className="rounded-div flex justify-between p-4 hover:scale-105 ease-in-out duration-300">
-            <div>
-              <div>
-                <img src={coin.item.small} alt="/" />
+            <div className="flex w-full items-center justify-between">
+              <div className="flex">
+                <img className="mr-4 rounded-full" src={coin.item.small} alt="/" />
                 <div>
-                  <p>{coin.item.name}</p>
+                  <p className=" font-bold">{coin.item.name}</p>
                   <p>{coin.item.symbol}</p>
                 </div>
               </div>
