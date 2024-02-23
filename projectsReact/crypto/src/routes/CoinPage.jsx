bab4f3c67a7c38644b549e27bcb548bea2fb7008
Michalkcb <michalkcb@gmail.com>
2024-02-23 20:01:10 +0100

    add mor data in ciun page

--- projectsReact/crypto/src/routes/CoinPage.jsx
+++ projectsReact/crypto/src/routes/CoinPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Sparklines, SparklinesLine } from "react-sparklines";
 
 const CoinPage = () => {
   const [coin, setCoin] = useState({});
@@ -26,11 +27,62 @@ const CoinPage = () => {
         <div>
           <div>
             {coin.market_data?.current_price ? (
-              <p>
-                {coin.market_data.current_price.pln.toLocaleString()} PLN
-              </p>
+              <p>{coin.market_data.current_price.pln.toLocaleString()} PLN</p>
             ) : null}
             <p>7 Day</p>
+          </div>
+          <div>
+            <Sparklines data={coin.market_data?.sparkline_7d.price}>
+              <SparklinesLine color="blue" />
+            </Sparklines>
+          </div>
+          <div>
+            <div>
+              <p>Market Cap</p>
+              {coin.market_data?.market_cap ? (
+                <p>{coin.market_data.market_cap.pln.toLocaleString()} PLN</p>
+              ) : null}
+            </div>
+            <div>
+              <p>Volume (24h) </p>
+              {coin.market_data?.market_cap ? (
+                <p>{coin.market_data.total_volume.pln.toLocaleString()} PLN</p>
+              ) : null}
+            </div>
+          </div>
+          <div>
+            <div>
+              <p>24h High </p>
+              {coin.market_data?.high_24h ? (
+                <p>{coin.market_data.high_24h.pln.toLocaleString()} PLN</p>
+              ) : null}
+            </div>
+            <div>
+              <p>24h Low </p>
+              {coin.market_data?.low_24h ? (
+                <p>{coin.market_data.low_24h.pln.toLocaleString()} PLN</p>
+              ) : null}
+            </div>
+          </div>
+        </div>
+        <div>
+          <p>
+            Market Stats
+          </p>
+          <div>
+            <div>
+              <p>Maret Rank</p>
+              {coin.market_cap_rank}
+            </div>
+            <div>
+              <p>Hashing Algorithm</p>
+              {coin.hashing_algorithm ? <p> {coin.hashing_algorithm} </p> : null}
+            </div>
+            <div>
+              <p className='text-gray-500 text-sm'>Trust Score</p>
+              {coin.tickers ? <p>{coin.liquidity_score}</p> : null}
+              {console.log(coin.tickers)}
+            </div>
           </div>
         </div>
       </div>
