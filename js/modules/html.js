1cbadbe4a2abeb03a3eb30bc41e7978d9f9ef063
Michalkcb <michalkcb@gmail.com>
2024-02-23 15:00:16 +0100

    change links in html projects

--- js/modules/html.js
+++ js/modules/html.js
@@ -1,11 +1,12 @@
 const projectsData = [
     {link: '/projectsHtml/glowingIcons/index.html', imageSrc: 'https://michalkcb.github.io/homePage/assets/glowingItems.jpg', alt: 'glowing_icons'},
-    {link: 'http://michalkcb.ct8.pl/new%20cruzeirosazuis/newTerminyPL.html',imageSrc: 'http://michalkcb.ct8.pl/pic/cruseiro.jpg',alt: 'page_pic1'},
-    {link: 'http://michalkcb.ct8.pl/glowingDiv/index.html',imageSrc: 'http://michalkcb.ct8.pl/glowingDiv/glowingDivs.png',alt: 'Glowing DIV'},
-    {link: '../old/index.html',imageSrc: 'http://michalkcb.ct8.pl/pic/old.png',alt: 'old WD'},
-    {link: 'http://michalkcb.ct8.pl/page07/page07.html',imageSrc: 'http://michalkcb.ct8.pl/pic/page07.png',alt: 'page_pic7'},
-    {link: 'http://michalkcb.ct8.pl/EuroChina/index.html',imageSrc: 'http://michalkcb.ct8.pl/pic/page02.png',alt: 'page_pic2'},
-    {link: 'http://michalkcb.ct8.pl/page06/page06.html',imageSrc: 'http://michalkcb.ct8.pl/pic/page06.png',alt: 'page_pic6'},
+    {link: '',imageSrc: 'https://michalkcb.github.io/homePage/pic/cruseiro.jpg',alt: 'cruzeirosazuis.eu'},
+    {link: 'https://michalkcb.github.io/homePage/projectsHtml/EuroChina/index.html',imageSrc: 'https://michalkcb.github.io/homePage/projectsHtml/EuroChina/img/eurochina.png',alt: 'EuroChina'},
+    {link: 'https://michalkcb.github.io/homePage/projectsHtml/glowingDiv/index.html',imageSrc: 'https://michalkcb.github.io/homePage/projectsHtml/glowingDiv/glowingDivs.png',alt: 'Glowing DIV'},
+    {link: 'https://michalkcb.github.io/homePage/page08/index.html',imageSrc: 'https://michalkcb.github.io/homePage/assets/webDesign.png',alt: 'old WD'},
+    {link: 'https://michalkcb.github.io/homePage/page07/page07.html',imageSrc: 'https://michalkcb.github.io/homePage/pic/page07.png',alt: 'TouYube'},
+    {link: 'https://michalkcb.github.io/homePage/page06/page06.html',imageSrc: 'https://michalkcb.github.io/homePage/pic/page06.png', alt: 'Foogle'},
+
     // {link: 'https://michalkcb.github.io/homePage/projectsHtml/semstorm/index.html',imageSrc: 'https://michalkcb.github.io/homePage/projectsHtml/semstorm/assets/semstorm.png',alt: 'semstorm'  }
   ];
 
