acd288ab4e7b47d6b293512a489b98cd84457c58
Michalkcb <michalkcb@gmail.com>
2024-02-23 15:32:26 +0100

    add currency to market value

--- projectsReact/crypto/src/components/CoinItem.jsx
+++ projectsReact/crypto/src/components/CoinItem.jsx
@@ -19,8 +19,8 @@ const CoinItem = ({ coin }) => {
       <td className=" uppercase">{coin.symbol}</td>
       <td>{coin.current_price.toLocaleString()} PLN</td>
       <td>{coin.price_change_percentage_24h > 0 ? (<p className=" text-green-600">{coin.price_change_percentage_24h.toFixed(2)}%</p>) : (<p className=" text-red-600">{coin.price_change_percentage_24h.toFixed(2)}%</p>)}</td>
-      <td className="w-[180px] hidden md:table-cell">{coin.total_volume}</td>
-      <td className="w-[180px] hidden md:table-cell">{coin.market_cap}</td>
+      <td className="w-[180px] hidden md:table-cell">{coin.total_volume.toLocaleString()} PLN</td>
+      <td className="w-[180px] hidden md:table-cell">{coin.market_cap.toLocaleString()} PLN</td>
       <td>
         <Sparklines data={coin.sparkline_in_7d.price}>
           <SparklinesLine color="blue" />
