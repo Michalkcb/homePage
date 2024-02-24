a57e0a74345f13181056fd24fb955eea8c6a9538
Michalkcb <michalkcb@gmail.com>
2024-02-24 19:23:30 +0100

    copy content from sign in to sign up page

--- projectsReact/crypto/src/routes/SignUp.jsx
+++ projectsReact/crypto/src/routes/SignUp.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiFillLock, AiOutlineMail } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div>
+    <div className='max-w-[400px] mx-auto min-h-[600px] px-4 py-20'>
+      <h1 className='text-2xl font-bold'>Sign In</h1>
+      <form>
+        <div className='my-4'>
+          <label>Email</label>
+          <div className='my-2 w-full relative rounded-2xl shadow-xl'>
+            <input className='w-full p-2 bg-primary border border-input rounded-2xl'
+              type='email'
+            />
+            <AiOutlineMail className='absolute right-2 top-3 text-gray-400' />
+          </div>
+        </div>
+        <div className='my-4'>
+          <label>Password</label>
+          <div className='my-2 w-full relative rounded-2xl shadow-xl'>
+            <input
+          
+              className='w-full p-2 bg-primary border border-input rounded-2xl'
+              type='password'
+            />
+            <AiFillLock className='absolute right-2 top-3 text-gray-400' />
+          </div>
+        </div>
+        <button className='w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl'>
+          Sign in
+        </button>
+      </form>
+      <p className='my-4'>
+        Don't have an account?{' '}
+        <Link to='/signup' className='text-accent'>
+          Sign up
+        </Link>
+      </p>
+    </div>
+  </div>
   )
 }
 
