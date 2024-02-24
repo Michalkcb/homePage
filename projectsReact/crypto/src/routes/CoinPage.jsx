4e5ff0d81162e158691b965283f21c216147765b
Michalkcb <michalkcb@gmail.com>
2024-02-24 17:43:27 +0100

    add 1y data

--- projectsReact/crypto/src/routes/CoinPage.jsx
+++ projectsReact/crypto/src/routes/CoinPage.jsx
@@ -88,6 +88,32 @@ const CoinPage = () => {
               <p>Price Change (24h)</p>
               {coin.market_data ? (<p>{coin.market_data.price_change_percentage_24h.toFixed(2)}%</p>) : (null)}
             </div>
+            <div>
+              <p>Price Change (7d)</p>
+              {coin.market_data ? (<p>{coin.market_data.price_change_percentage_7d.toFixed(2)}%</p>) : (null)}
+            </div>
+            <div>
+              <p>Price Change (14d)</p>
+              {coin.market_data ? (<p>{coin.market_data.price_change_percentage_14d.toFixed(2)}%</p>) : (null)}
+            </div>
+          </div>
+          <div>
+            <div>
+            <p>Price Change (30d)</p>
+              {coin.market_data ? (<p>{coin.market_data.price_change_percentage_30d.toFixed(2)}%</p>) : (null)}
+            </div>
+            <div>
+            <p>Price Change (60d)</p>
+              {coin.market_data ? (<p>{coin.market_data.price_change_percentage_60d.toFixed(2)}%</p>) : (null)}
+            </div>
+            <div>
+            <p>Price Change (1y)</p>
+              {coin.market_data ? (<p>{coin.market_data.price_change_percentage_1y.toFixed(2)}%</p>) : (null)}
+            </div>
+          </div>
+
+          <div>
+            
           </div>
         </div>
       </div>
