ff2b95f69aa45d159f71415d7e365fdd9a895806
Michalkcb <michalkcb@gmail.com>
2024-02-24 19:28:10 +0100

    Start adding account page lements

--- projectsReact/crypto/src/routes/Account.jsx
+++ projectsReact/crypto/src/routes/Account.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <div>
+      <div>
+        <div>
+          <h1>Account</h1>
+          <div>
+            <p>Welcome, user</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
