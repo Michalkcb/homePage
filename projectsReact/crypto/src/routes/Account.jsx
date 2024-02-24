2b061fe6128ed7803d7f925f11d9b2600391a27b
Michalkcb <michalkcb@gmail.com>
2024-02-24 20:06:58 +0100

    add elements and styles to account route

--- projectsReact/crypto/src/routes/Account.jsx
+++ projectsReact/crypto/src/routes/Account.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import SavedCoin from '../components/SavedCoin'
+import React from "react";
+import SavedCoin from "../components/SavedCoin";
 
 const Account = () => {
   return (
-    <div>
-      <div>
+    <div className="max-w-[1140px] mx-auto">
+      <div className="flex justify-between items-center my-12 py-8 rounded-div">
         <div>
-          <h1>Account</h1>
+          <h1 className="text-2xl font-bold">Account</h1>
           <div>
             <p>Welcome, user</p>
           </div>
         </div>
         <div>
-          <button>Sign Out</button>
+          <button className="border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl">
+            Sign Out
+          </button>
         </div>
-      <div>
-        <h1>Watch List</h1>
-        <SavedCoin/>
       </div>
+      <div className="flex justfiy-between items-center my-12 py-8 rounded-div">
+        <div className="w-full min-h-[300px]">
+          <h1 className="text-2xl font-bold py-4">Watch List</h1>
+          <SavedCoin />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
