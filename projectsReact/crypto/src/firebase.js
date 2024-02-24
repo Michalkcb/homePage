4967146161168a8db1f4bbb82334e05e65188d42
Michalkcb <michalkcb@gmail.com>
2024-02-24 20:23:38 +0100

    MAKE ENV FILE WITH FIREBASE KEYS

--- projectsReact/crypto/src/firebase.js
+++ projectsReact/crypto/src/firebase.js
@@ -7,12 +7,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBF2HosSdsBZ0th05j0Lhx1l0QN8HJVeOE",
-  authDomain: "mb-crypto-hub.firebaseapp.com",
-  projectId: "mb-crypto-hub",
-  storageBucket: "mb-crypto-hub.appspot.com",
-  messagingSenderId: "404198622476",
-  appId: "1:404198622476:web:f302acf96f86d124db3529",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER,
+  appId: REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-VDYJJ9D1ZY"
 };
 
