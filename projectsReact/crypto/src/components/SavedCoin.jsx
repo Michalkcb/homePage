b3aee6188af7a6a55a26ea1d5cb2414e1d1ceb3b
Michalkcb <michalkcb@gmail.com>
2024-02-24 19:56:26 +0100

    set saved coin list length = 0

--- projectsReact/crypto/src/components/SavedCoin.jsx
+++ projectsReact/crypto/src/components/SavedCoin.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SavedCoin = () => {
+    const [coins, setCoins] = useState
   return (
     <div>
-      <p>
+        {coins.lenght === 0 ? (<p>
         You don't have any coins saved. Please save a coin to add it to your
         watch list. <Link to="/">Click here to search coins.</Link>
-      </p>
+      </p>) : (
+        <table>
+            
+        </table>
+      )}
+      
     </div>
   );
 };
