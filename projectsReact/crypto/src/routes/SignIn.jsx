3de502fb6f390dec07dffa5211aa9e780133b55d
Michalkcb <michalkcb@gmail.com>
2024-02-25 19:48:38 +0100

    add email on click

--- projectsReact/crypto/src/routes/SignIn.jsx
+++ projectsReact/crypto/src/routes/SignIn.jsx
@@ -31,7 +31,9 @@ const Signin = () => {
           <div className='my-4'>
             <label>Email</label>
             <div className='my-2 w-full relative rounded-2xl shadow-xl'>
-              <input className='w-full p-2 bg-primary border border-input rounded-2xl'
+              <input 
+              onChange={(e) => {setEmail(e.target.value)}}
+              className='w-full p-2 bg-primary border border-input rounded-2xl'
                 type='email'
               />
               <AiOutlineMail className='absolute right-2 top-3 text-gray-400' />
