87e918aa9dc878ab05c6a884eebfdfbcf99f0115
Michalkcb <michalkcb@gmail.com>
2024-02-25 14:31:57 +0100

    add <userContrxt.provider>

--- projectsReact/crypto/src/context/AuthContext.jsx
+++ projectsReact/crypto/src/context/AuthContext.jsx
@@ -8,3 +8,40 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
+
+const UserContext = useContext();
+
+export const AuthContextProvider = ({ children }) => {
+const [user, setUser] = useState({});
+
+  const signUp = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password);
+    return (
+      setDoc(db, "users", email),
+      {
+        watchList: [],
+      }
+    );
+  };
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password)
+  };
+  const logout = () => {
+    return signOut(auth)
+  };
+
+  useEffect(()=>{
+    const unsubscribe = onAuthStateChanged(auth, (currentUser)=> {
+        setUser(currentUser)
+    })
+    return ()=> {
+        unsubscribe();
+    }
+  },[])
+  return(
+    <UserContext.Provider value={{signUp, signIn, logout, user}}>
+        {children}
+    </UserContext.Provider>
+
+  )
+};
