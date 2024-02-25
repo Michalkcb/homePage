1fef190ec4f0243a37d296ec7697a8a527f28361
Michalkcb <michalkcb@gmail.com>
2024-02-25 17:23:32 +0100

    solve firebasa authentication but database is not working

--- projectsReact/crypto/src/context/AuthContext.jsx
+++ projectsReact/crypto/src/context/AuthContext.jsx
@@ -1,49 +1,47 @@
-import { createContext, useContext, useState, useEffect } from "react";
-import React from "react";
-import { auth, db } from "../firebase";
+import { createContext, useContext, useState, useEffect } from 'react';
+import { auth, db } from '../firebase';
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
-} from "firebase/auth";
-import { doc, setDoc } from "firebase/firestore";
+} from 'firebase/auth';
+
+import { doc, setDoc } from 'firebase/firestore';
 
 const UserContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-const [user, setUser] = useState({});
+  const [user, setUser] = useState({});
 
   const signUp = (email, password) => {
     createUserWithEmailAndPassword(auth, email, password);
-    return (
-      setDoc(db, "users", email),
-      {
-        watchList: [],
-      }
-    );
+    return setDoc(doc(db, 'users', email), {
+      watchList: [],
+    });
   };
   const signIn = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password)
+    return signInWithEmailAndPassword(auth, email, password);
   };
+
   const logout = () => {
-    return signOut(auth)
+    return signOut(auth);
   };
 
-  useEffect(()=>{
-    const unsubscribe = onAuthStateChanged(auth, (currentUser)=> {
-        setUser(currentUser)
-    })
-    return ()=> {
-        unsubscribe();
-    }
-  },[])
-  return(
-    <UserContext.Provider value={{signUp, signIn, logout, user}}>
-        {children}
-    </UserContext.Provider>
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
-  )
+  return (
+    <UserContext.Provider value={{ signUp, signIn, logout, user }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export const UserAuth = () => {
