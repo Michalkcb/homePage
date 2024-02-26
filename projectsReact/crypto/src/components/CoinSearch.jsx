33ac62dbae5a9e91863a2d658c71f4bc31ba7e29
Michalkcb <michalkcb@gmail.com>
2024-02-26 20:24:49 +0100

    change crypto to Crypto

--- projectsReact/crypto/src/components/CoinSearch.jsx
+++ projectsReact/crypto/src/components/CoinSearch.jsx
@@ -8,7 +8,7 @@ const CoinSearch = ({ coins }) => {
   return (
     <div className=" rounded-div my-24">
       <div className="flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
-        <h1 className="text-2xl font-bold my-2">Search crypto</h1>
+        <h1 className="text-2xl font-bold my-2">Search Crypto</h1>
         <form>
           <input onChange={(e)=>setSearchText(e.target.value)} 
           className=" w-full bg-primary border border-imput px-4 py-2 rounded-2xl shadow-xl" type="text" placeholder="Search a coin" />
