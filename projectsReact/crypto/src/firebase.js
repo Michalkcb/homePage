8674c06248be8c03e92e1069f48836f7c6d5eec6
Michalkcb <michalkcb@gmail.com>
2024-02-29 17:13:52 +0100

    creating save file env

--- projectsReact/crypto/src/firebase.js
+++ projectsReact/crypto/src/firebase.js
@@ -10,13 +10,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBF2HosSdsBZ0th05j0Lhx1l0QN8HJVeOE",
-  authDomain: "mb-crypto-hub.firebaseapp.com",
-  projectId: "mb-crypto-hub",
-  storageBucket: "mb-crypto-hub.appspot.com",
-  messagingSenderId: "404198622476",
-  appId: "1:404198622476:web:f302acf96f86d124db3529",
-  measurementId: "G-VDYJJ9D1ZY"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
