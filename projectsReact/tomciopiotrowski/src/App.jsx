f33b4a95237e8c923efd7aad73be664573380219
Michalkcb <michalkcb@gmail.com>
2024-03-10 20:54:39 +0100

    styling font

--- projectsReact/tomciopiotrowski/src/App.jsx
+++ projectsReact/tomciopiotrowski/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline bg-green-500">
+    <h1 className="text-3xl bg-green-500 text-gray-500 font-mono uppercase tracking-wide tracking-2xl">
       Tomcio Piotrowski
     </h1>
     </>
--- projectsReact/tomciopiotrowski/tailwind.config.js
+++ projectsReact/tomciopiotrowski/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: [ 'Figtree', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
