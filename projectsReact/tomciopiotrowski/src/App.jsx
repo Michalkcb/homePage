68bcae6235ad5da5b5ba9f9cf2ab338659063642
Michalkcb <michalkcb@gmail.com>
2024-03-12 20:17:10 +0100

    import routes

--- projectsReact/tomciopiotrowski/src/App.jsx
+++ projectsReact/tomciopiotrowski/src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./routes/Home";
 import GaleryStart from "./routes/GaleryStart";
+import About from "./routes/About";
+import Objects from "./routes/Objects";
+import Movie from "./routes/Movie";
+import People from "./routes/People";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<About />} />
       </Routes>
       <GaleryStart />
     </Router>
