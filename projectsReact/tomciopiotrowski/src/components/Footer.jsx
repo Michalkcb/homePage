f8fcf4ba3bfeafa5b2818a5d1b88fbeae2b9daea
Michalkcb <michalkcb@gmail.com>
2024-03-13 21:20:57 +0100

    remove unused import

--- projectsReact/tomciopiotrowski/src/components/Footer.jsx
+++ projectsReact/tomciopiotrowski/src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { AiOutlineClose, AiFillFacebook } from "react-icons/ai";
 import { FaSquareXTwitter, FaInstagram, FaFacebookF, FaSnapchat } from "react-icons/fa6";
 
 const Footer = () => {
