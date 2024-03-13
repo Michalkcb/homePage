ff0430fa0df8bc54746134207d61b05514b35dd4
Michalkcb <michalkcb@gmail.com>
2024-03-13 22:13:22 +0100

    add generating gallery to galery start

--- projectsReact/tomciopiotrowski/src/components/GaleryItem.jsx
+++ projectsReact/tomciopiotrowski/src/components/GaleryItem.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const GaleryItem = ({ img }) => {
   return (
-    <div className=" flex gap-4 flex-wrap mt-[80px]">
-      <div className=' mx-4'>
-        <img src={img} alt="image" className="rounded-xl" />
-      </div>
+    <div className=" mx-4">
+      <img src={img} alt="image" className="rounded-xl" />
     </div>
   );
 };
