fc05f0d859a4d70163ee918329f4093a8b38532b
Michalkcb <michalkcb@gmail.com>
2024-03-13 22:26:49 +0100

    people generate galery

--- projectsReact/tomciopiotrowski/src/routes/People.jsx
+++ projectsReact/tomciopiotrowski/src/routes/People.jsx
@@ -1,25 +1,16 @@
 import React from 'react'
+import PeopleData from './PeopleData'
+import GaleryItem from '../components/GaleryItem'
+
+
 
 const People = () => {
   return (
-    <div className=' flex gap-4 flex-wrap mt-[80px]'>
-        <div className=' mx-4'>
-            <img 
-            className=' rounded-xl'
-            src='https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/410231018_10159233240315563_7847957630435191504_n.jpg' alt="pic01" />
-        </div>
-        <div className='mx-4'>
-            <img 
-            className='rounded-xl'
-            src='https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/410293550_10159233286100563_5494337073556784863_n.jpg' alt="pic01" />
-        </div>
-        <div className='mx-4'>
-            <img 
-            className='rounded-xl'
-            src='https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/410298603_10159233263565563_7734816224702279160_n.jpg' alt="pic01" />
-        </div>
-
-    </div>
+    <div className=" flex gap-4 flex-wrap mt-[80px]">
+    {PeopleData.map((item, idy) => (
+      <GaleryItem key={idy} img={item.imageSrc} />
+    ))}
+  </div>
   )
 }
 
--- projectsReact/tomciopiotrowski/src/routes/PeopleData.jsx
+++ projectsReact/tomciopiotrowski/src/routes/PeopleData.jsx
@@ -1,28 +1,16 @@
-const GaleryStartData = [
+const PeopleData = [
   {
     imageSrc:
-      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/347250423_10158946128525563_6717665936168975433_n.jpg",
+      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/410231018_10159233240315563_7847957630435191504_n.jpg",
   },
   {
     imageSrc:
-      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/410815943_10159233251425563_4459431366362872379_n.jpg",
+      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/410293550_10159233286100563_5494337073556784863_n.jpg",
   },
   {
     imageSrc:
-      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/356923609_10158949128505563_6137640043970528478_n.jpg",
-  },
-  {
-    imageSrc:
-      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/368187348_10159033876035563_5684618983903585155_n.jpg",
-  },
-  {
-    imageSrc:
-      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/410812104_10159233289080563_7965221519588041280_n.jpg",
-  },
-  {
-    imageSrc:
-      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/418934826_10159265442855563_8114150926809586284_n.jpg",
+      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/410298603_10159233263565563_7734816224702279160_n.jpg",
   },
 ];
 
-export default GaleryStartData;
+export default PeopleData;
