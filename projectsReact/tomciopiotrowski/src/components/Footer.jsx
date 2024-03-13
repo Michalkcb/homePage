dc2156b5fab0967059980242b415419001879a61
Michalkcb <michalkcb@gmail.com>
2024-03-13 21:35:14 +0100

    add links to social

--- projectsReact/tomciopiotrowski/src/components/Footer.jsx
+++ projectsReact/tomciopiotrowski/src/components/Footer.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { FaSquareXTwitter, FaInstagram, FaFacebookF, FaSnapchat } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <div className='bg-gray-800 h-[60px] fixed bottom-0 w-full opacity-100 flex justify-around items-center text-white'>
+      <Link to='https://www.facebook.com/tomciopiotrowski' target='_blank'>
       <FaFacebookF size={30} />
+      </Link>
+      <Link to='https://www.facebook.com/tomaspiotrowski/?locale=pl_PL' target='_blank'>
       <FaSquareXTwitter size={30} />
+      </Link>
+      <Link to='https://www.instagram.com/wedding_tomas_piotrowski' target='_blank'>
       <FaInstagram size={30} />
+      </Link>
+      <Link to='https://www.facebook.com/tomciopiotrowski' target='_blank'>
       <FaSnapchat size={30} />
+      </Link>
     </div>
   )
 }
