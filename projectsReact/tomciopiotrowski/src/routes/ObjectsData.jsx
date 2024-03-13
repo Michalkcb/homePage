c889288dc9873ec6a93c4e3e66bbb3a2926e81f8
Michalkcb <michalkcb@gmail.com>
2024-03-13 22:18:55 +0100

    create other galerys data

---
+++ projectsReact/tomciopiotrowski/src/routes/ObjectsData.jsx
@@ -0,0 +1,28 @@
+const GaleryStartData = [
+  {
+    imageSrc:
+      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/347250423_10158946128525563_6717665936168975433_n.jpg",
+  },
+  {
+    imageSrc:
+      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/410815943_10159233251425563_4459431366362872379_n.jpg",
+  },
+  {
+    imageSrc:
+      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/356923609_10158949128505563_6137640043970528478_n.jpg",
+  },
+  {
+    imageSrc:
+      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/368187348_10159033876035563_5684618983903585155_n.jpg",
+  },
+  {
+    imageSrc:
+      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/410812104_10159233289080563_7965221519588041280_n.jpg",
+  },
+  {
+    imageSrc:
+      "https://michalkcb.github.io/homePage/projectsReact/tomciopiotrowski/src/assets/418934826_10159265442855563_8114150926809586284_n.jpg",
+  },
+];
+
+export default GaleryStartData;
