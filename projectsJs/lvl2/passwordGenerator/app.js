5783167547eca4dc9088481a75040e8ab9493d79
Michalkcb <michalkcb@gmail.com>
2024-04-11 18:49:55 +0200

    add dark mode

--- projectsJs/lvl2/passwordGenerator/app.js
+++ projectsJs/lvl2/passwordGenerator/app.js
@@ -1,13 +1,12 @@
 const passwordElement = document.getElementById("password");
 const rangeBar = document.getElementById("rangeBar");
 const rangeInput = document.getElementById("rangeInput");
-
 const form = document.getElementById("generatorForm");
-
 const uppercase = document.querySelector("#uppercase");
 const numbers = document.querySelector("#numbers");
 const symbols = document.querySelector("#symbols");
-const submit = document.querySelector("#buttton");
+const submit = document.querySelector("#button");
+const themeToggle = document.getElementById("themeToggle");
 
 const arryFromChartCode = (start, end) => {
   const arry = [];
@@ -22,7 +21,6 @@ const uppercaseCodes = arryFromChartCode(65, 90);
 const numbersCodes = arryFromChartCode(48, 57);
 const symbolsCodes = arryFromChartCode(33, 47).concat(arryFromChartCode(58,64)).concat(uppercaseCodes).concat(arryFromChartCode(91,96)).concat(arryFromChartCode(123, 126))
 
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const longness = rangeInput.value;
@@ -33,7 +31,8 @@ form.addEventListener("submit", (e) => {
 const password =  generatedPass(symbolsChecked,numbersChecked,uppercaseChecked,longness)
 passwordElement.innerHTML = password
 })
-  function  generatedPass(symbolsChecked,numbersChecked,uppercaseChecked,longness) {
+
+function  generatedPass(symbolsChecked,numbersChecked,uppercaseChecked,longness) {
     let passwordCodes=lowercaseCodes
     if (uppercaseChecked) passwordCodes= passwordCodes.concat(uppercaseCodes)
     if (numbersChecked) passwordCodes=passwordCodes.concat(numbersCodes)
@@ -55,3 +54,7 @@ function passLong(e) {
 
 rangeBar.addEventListener("input", passLong);
 rangeInput.addEventListener("input", passLong);
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
