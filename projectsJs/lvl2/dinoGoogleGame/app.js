8329a5e486e26a5882cac6c28e0e6051293b69c4
Michalkcb <michalkcb@gmail.com>
2024-05-27 23:56:37 +0200

    upload to homepage

--- projectsJs/lvl2/dinoGoogleGame/app.js
+++ projectsJs/lvl2/dinoGoogleGame/app.js
@@ -1,30 +1,33 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
-var counter=0;
+var counter = 0;
 
-const jump = () =>{
-    if(character.classList == "animate"){return}
+const jump = () => {
+    if (character.classList.contains("animate")) { return }
     character.classList.add("animate");
-    setTimeout(function(){
+    setTimeout(function () {
         character.classList.remove("animate");
-    },300);
+    }, 300);
 }
-var checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
 
+var checkDead = setInterval(function () {
+    let characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue("bottom"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
 
-    if(
-        blockLeft<20 && blockLeft>-20 && characterTop>=130){
+    // Zaktualizowane wartości do detekcji kolizji
+    if (blockLeft < 120 && blockLeft > 50 && characterBottom < 40) { // Zmieniono warunki kolizji, aby uwzględnić nowe wymiary
         block.style.animation = "none";
-
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
+        alert("Game Over. score: " + Math.floor(counter / 100));
+        counter = 0;
         block.style.animation = "block 1s infinite linear";
-    }else{
+    } else {
         counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
     }
 }, 10);
 
 document.addEventListener('keydown', jump);
+
+document.getElementById('toggle-theme').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
