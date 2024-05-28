75c29abad6984153b1bd6b609c96ac87394f6e22
Michalkcb <michalkcb@gmail.com>
2024-05-28 23:33:54 +0200

    new wersion

--- projectsJs/lvl2/dinoGoogleGame/app.js
+++ projectsJs/lvl2/dinoGoogleGame/app.js
@@ -3,31 +3,38 @@ var block = document.getElementById("block");
 var counter = 0;
 
 const jump = () => {
-    if (character.classList.contains("animate")) { return }
-    character.classList.add("animate");
-    setTimeout(function () {
-        character.classList.remove("animate");
-    }, 300);
-}
+  if (character.classList.contains("animate")) {
+    return;
+  }
+  character.classList.add("animate");
+  setTimeout(function () {
+    character.classList.remove("animate");
+  }, 300);
+};
 
 var checkDead = setInterval(function () {
-    let characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue("bottom"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+  let characterBottom = parseInt(
+    window.getComputedStyle(character).getPropertyValue("bottom")
+  );
+  let blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
 
-    // Zaktualizowane wartości do detekcji kolizji
-    if (blockLeft < 120 && blockLeft > 50 && characterBottom < 40) { // Zmieniono warunki kolizji, aby uwzględnić nowe wymiary
-        block.style.animation = "none";
-        alert("Game Over. score: " + Math.floor(counter / 100));
-        counter = 0;
-        block.style.animation = "block 1s infinite linear";
-    } else {
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
-    }
+  // colision
+  if (blockLeft < 120 && blockLeft > 50 && characterBottom < 40) {
+    block.style.animation = "none";
+    alert("Game Over. score: " + Math.floor(counter / 100));
+    counter = 0;
+    block.style.animation = "block 1s infinite linear";
+  } else {
+    counter++;
+    document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
+  }
 }, 10);
 
-document.addEventListener('keydown', jump);
+document.addEventListener("keydown", jump);
+document.addEventListener("touchstart", jump);
 
-document.getElementById('toggle-theme').addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
+document.getElementById("toggle-theme").addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
 });
